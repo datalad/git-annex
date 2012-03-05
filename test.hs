1e2f55911a04eb0299af88bb545c0ed57d410cc4
Joey Hess <joey@kitenet.net>
2012-03-05 12:42:52 -0400

    Fix a bug in symlink calculation code, that triggered in rare cases where an annexed file is in a subdirectory that nearly matched to the .git/annex/object/xx/yy subdirectories.
    This is a straight up pure-code stinker. The relative path calculation
    looked for common subdirectories in the two paths, but failed to stop
    after the paths diverged. When a later pair of subdirectories were the
    same, the resulting relative path was wrong.
    
    Added regression test for this.
    (cherry picked from commit 51338486dcf9ab86de426e41b1eb31af1d3a6c87)
    
    Conflicts:
    
    	debian/changelog

--- test.hs
+++ test.hs
@@ -82,6 +82,7 @@ quickcheck = TestLabel "quickcheck" $ TestList
 	, qctest "prop_parentDir_basics" Utility.Path.prop_parentDir_basics
 
 	, qctest "prop_relPathDirToFile_basics" Utility.Path.prop_relPathDirToFile_basics
+	, qctest "prop_relPathDirToFile_regressionTest" Utility.Path.prop_relPathDirToFile_regressionTest
 	, qctest "prop_cost_sane" Config.prop_cost_sane
 	, qctest "prop_hmacWithCipher_sane" Crypto.prop_hmacWithCipher_sane
 	, qctest "prop_TimeStamp_sane" Logs.UUIDBased.prop_TimeStamp_sane
