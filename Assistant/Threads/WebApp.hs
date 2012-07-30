78b3dada5b056320922cb9f453343ae4d95b0407
Joey Hess <joey@kitenet.net>
2012-07-30 14:50:32 -0400

    better connection close handling
    Depending on how the webapp was started up and whether the user clicked on
    any links in it, window.close() may be disallowed by browser security
    policy. Also if that fails, display a modal dialog that nicely blackens out
    the webapp.
    
    TODO: avoid Escape closing it. Bootstrap's docs are unclear about how to do
    that.

--- Assistant/Threads/WebApp.hs
+++ Assistant/Threads/WebApp.hs
@@ -115,6 +115,7 @@ instance Yesod WebApp where
 			addStylesheet $ StaticR css_bootstrap_responsive_css
 			addScript $ StaticR jquery_full_js
 			addScript $ StaticR js_bootstrap_dropdown_js
+			addScript $ StaticR js_bootstrap_modal_js
 			$(widgetFile "page")
 		hamletToRepHtml $(hamletFile $ hamletTemplate "bootstrap")
 
