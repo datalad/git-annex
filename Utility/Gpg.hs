datalad/git-annex

commit d1da9cf221aeea5c7ac8a313a18b559791a04f12

switch from System.Cmd.Utils to System.Process
Test suite now passes with -threaded!

I traced back all the hangs with -threaded to System.Cmd.Utils. It seems
it's just crappy/unsafe/outdated, and should not be used. System.Process
seems to be the cool new thing, so converted all the code to use it
instead.

In the process, --debug stopped printing commands it runs. I may try to
bring that back later.

Note that even SafeSystem was switched to use System.Process. Since that
was a modified version of code from System.Cmd.Utils, it needed to be
converted too. I also got rid of nearly all calls to forkProcess,
and all calls to executeFile, which I'm also doubtful about working
well with -threaded.

Utility/Gpg.hs
@@ -11,9 +11,9 @@ import qualified Data.ByteString.Lazy as L
 import System.Posix.Types
 import Control.Applicative
 import Control.Concurrent
-import Control.Exception (finally, bracket)
-import System.Exit
+import Control.Exception (bracket)
 import System.Posix.Env (setEnv, unsetEnv, getEnv)	
+import System.Process
 
 import Common
 
@@ -39,18 +39,30 @@ stdParams params = do
 readStrict :: [CommandParam] -> IO String
 readStrict params = do
 	params' <- stdParams params
-	pOpen ReadFromPipe "gpg" params' hGetContentsStrict
+	(_, Just from, _, pid)
+		<- createProcess (proc "gpg" params')
+			{ std_out = CreatePipe }
+	hSetBinaryMode from True
+	r <- hGetContentsStrict from
+	forceSuccessProcess pid "gpg" params'
+	return r
 
 {- Runs gpg, piping an input value to it, and returning its stdout,
  - strictly. -}
 pipeStrict :: [CommandParam] -> String -> IO String
 pipeStrict params input = do
 	params' <- stdParams params
-	(pid, fromh, toh) <- hPipeBoth "gpg" params'
-	_ <- forkIO $ finally (hPutStr toh input) (hClose toh)
-	output <- hGetContentsStrict fromh
-	forceSuccess pid
-	return output
+	(Just to, Just from, _, pid)
+		<- createProcess (proc "gpg" params')
+			{ std_in = CreatePipe
+			, std_out = CreatePipe }
+	hSetBinaryMode to True
+	hSetBinaryMode from True
+	hPutStr to input
+	hClose to
+	r <- hGetContentsStrict from
+	forceSuccessProcess pid "gpg" params'
+	return r
 
 {- Runs gpg with some parameters, first feeding it a passphrase via
  - --passphrase-fd, then feeding it an input, and passing a handle
@@ -70,17 +82,14 @@ passphraseHandle params passphrase a b = do
 	let passphrasefd = [Param "--passphrase-fd", Param $ show pfd]
 
 	params' <- stdParams $ passphrasefd ++ params
-	(pid, fromh, toh) <- hPipeBoth "gpg" params'
-	pid2 <- forkProcess $ do
-		L.hPut toh =<< a
-		hClose toh
-		exitSuccess
+	(Just toh, Just fromh, _, pid) <- createProcess (proc "gpg" params')
+		{ std_in = CreatePipe, std_out = CreatePipe }
+	L.hPut toh =<< a
 	hClose toh
 	ret <- b fromh
 
 	-- cleanup
-	forceSuccess pid
-	_ <- getProcessStatus True False pid2
+	forceSuccessProcess pid "gpg" params'
 	closeFd frompipe
 	return ret
 

Annex/Branch.hs
@@ -164,9 +164,7 @@ get' staleok file = fromcache =<< getCache file
 		fromjournal Nothing
 			| staleok = withIndex frombranch
 			| otherwise = withIndexUpdate $ frombranch >>= cache
-		frombranch = do
-			liftIO $ putStrLn $ "frombranch " ++ file
-			L.unpack <$> catFile fullname file
+		frombranch = L.unpack <$> catFile fullname file
 		cache content = do
 			setCache file content
 			return content

Annex/UUID.hs
@@ -20,6 +20,8 @@ module Annex.UUID (
 	removeRepoUUID,
 ) where
 
+import System.Process
+
 import Common.Annex
 import qualified Git
 import qualified Git.Config
@@ -32,8 +34,10 @@ configkey = annexConfig "uuid"
 {- Generates a UUID. There is a library for this, but it's not packaged,
  - so use the command line tool. -}
 genUUID :: IO UUID
-genUUID = pOpen ReadFromPipe command params $ liftM toUUID . hGetLine
+genUUID = gen . lines <$> readProcess command params []
 	where
+		gen [] = error $ "no output from " ++ command
+		gen (l:_) = toUUID l
 		command = SysConfig.uuid
 		params
 			-- request a random uuid be generated

Backend/SHA.hs
@@ -12,6 +12,7 @@ import qualified Annex
 import Types.Backend
 import Types.Key
 import Types.KeySource
+import System.Process
 
 import qualified Build.SysConfig as SysConfig
 import Data.Digest.Pure.SHA
@@ -53,14 +54,16 @@ shaN shasize file filesize = do
 	showAction "checksum"
 	case shaCommand shasize filesize of
 		Left sha -> liftIO $ sha <$> L.readFile file
-		Right command -> liftIO $ runcommand command
+		Right command -> liftIO $ parse command . lines <$>
+			readProcess command (toCommand [File file]) ""
 	where
-		runcommand command =
-			pOpen ReadFromPipe command (toCommand [File file]) $ \h -> do
-				sha <- fst . separate (== ' ') <$> hGetLine h
-				if null sha
-					then error $ command ++ " parse error"
-					else return sha
+		parse command [] = bad command
+		parse command (l:_)
+			| null sha = bad command
+			| otherwise = sha
+			where
+				sha = fst $ separate (== ' ') l
+		bad command = error $ command ++ " parse error"
 
 shaCommand :: SHASize -> Integer -> Either (L.ByteString -> String) String
 shaCommand shasize filesize

Build/Configure.hs
@@ -4,7 +4,7 @@ module Build.Configure where
 
 import System.Directory
 import Data.List
-import System.Cmd.Utils
+import System.Process
 import Control.Applicative
 import System.FilePath
 
@@ -71,7 +71,7 @@ getVersionString = do
 
 getGitVersion :: Test
 getGitVersion = do
-	(_, s) <- pipeFrom "git" ["--version"]
+	s <- readProcess "git" ["--version"] ""
 	let version = unwords $ drop 2 $ words $ head $ lines s
 	return $ Config "gitversion" (StringConfig version)
 

Command/Fsck.hs
@@ -7,6 +7,8 @@
 
 module Command.Fsck where
 
+import System.Posix.Process (getProcessID)
+
 import Common.Annex
 import Command
 import qualified Annex

Command/Map.hs
@@ -9,6 +9,7 @@ module Command.Map where
 
 import Control.Exception.Extensible
 import qualified Data.Map as M
+import System.Process
 
 import Common.Annex
 import Command
@@ -198,9 +199,13 @@ tryScan r
 			case result of
 				Left _ -> return Nothing
 				Right r' -> return $ Just r'
-		pipedconfig cmd params = safely $
-			pOpen ReadFromPipe cmd (toCommand params) $
-				Git.Config.hRead r
+		pipedconfig cmd params = safely $ do
+			(_, Just h, _, pid) <-
+				createProcess (proc cmd $ toCommand params)
+					{ std_out = CreatePipe }
+			r' <- Git.Config.hRead r h
+			forceSuccessProcess pid cmd $ toCommand params
+			return r'
 
 		configlist =
 			onRemote r (pipedconfig, Nothing) "configlist" [] []

Common.hs
@@ -13,16 +13,15 @@ import Data.String.Utils as X
 import System.Path as X
 import System.FilePath as X
 import System.Directory as X
-import System.Cmd.Utils as X hiding (safeSystem)
 import System.IO as X hiding (FilePath)
 import System.Posix.Files as X
 import System.Posix.IO as X
-import System.Posix.Process as X hiding (executeFile)
 import System.Exit as X
 
 import Utility.Misc as X
 import Utility.Exception as X
 import Utility.SafeCommand as X
+import Utility.Process as X
 import Utility.Path as X
 import Utility.Directory as X
 import Utility.Monad as X

commit c6f3c410d4f555fe2c989780ddbddd6b37e59096

fix gpg pipeline stall
Bug introduced in d1da9cf221aeea5c7ac8a313a18b559791a04f12, where
I removed a forkProcess when feeding gpg. Feeding it from a thread
solves the bug.

Utility/Gpg.hs
@@ -64,7 +64,7 @@ passphraseHandle :: [CommandParam] -> String -> IO L.ByteString -> (Handle -> IO
 passphraseHandle params passphrase a b = do
 	-- pipe the passphrase into gpg on a fd
 	(frompipe, topipe) <- createPipe
-	_ <- forkIO $ do
+	void $ forkIO $ do
 		toh <- fdToHandle topipe
 		hPutStrLn toh passphrase
 		hClose toh
@@ -76,8 +76,9 @@ passphraseHandle params passphrase a b = do
 		withBothHandles createProcessSuccess (proc "gpg" params') go
 	where
 		go (to, from) = do
-			L.hPut to =<< a
-			hClose to
+			void $ forkIO $ do
+				L.hPut to =<< a
+				hClose to
 			b from
 
 {- Finds gpg public keys matching some string. (Could be an email address,

doc/bugs/transferkey_fails_due_to_gpg.mdwn
@@ -43,3 +43,9 @@ The key which is used for the remote is password protected. The GnuPG Agent asks
 When adding a new rsync-remote with encryption set to 'none' (therefore disabled), everything works, so it really seems a gpg issue.
 
 How can I help debugging?
+
+> Thanks, I reproduced a deadlock in the gpg code, which was introduced
+> with some of my earlier changes to use threading. No released version of
+> git-annex was affected, and I have developed a fix, which works for me
+> and is now committed to master. Marking this [[done]]; please do check
+> that my fix works for you! --[[Joey]]