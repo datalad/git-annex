f06587ba23662d33dc11a1096cad41a776820ff0
Joey Hess <joey@kitenet.net>
2012-10-09 15:11:48 -0400

    basic repository edit form
    Only shows description so far.

--- Assistant/WebApp/Configurators/Edit.hs
+++ Assistant/WebApp/Configurators/Edit.hs
@@ -13,31 +13,44 @@ import Assistant.Common
 import Assistant.WebApp
 import Assistant.WebApp.Types
 import Assistant.WebApp.SideBar
-import Assistant.Sync
-import Assistant.MakeRemote
 import Utility.Yesod
-import Init
-import qualified Git
-import qualified Git.Construct
-import qualified Git.Config
-import qualified Annex
-import Locations.UserConfig
-import Utility.FreeDesktop
-import Utility.Mounts
-import Utility.DiskFree
-import Utility.DataUnits
-import Utility.Network
-import Remote (prettyListUUIDs)
-import Logs.Group
-import Annex.UUID
+import qualified Remote
+import Logs.UUID
 
 import Yesod
 import Data.Text (Text)
 import qualified Data.Text as T
-import qualified Data.Set as S
-import Data.Char
-import System.Posix.Directory
-import qualified Control.Exception as E
+import qualified Data.Map as M
+
+data RepoConfig = RepoConfig
+	{ repoDescription :: Text
+	}
+	deriving (Show)
+
+editRepositoryAForm :: RepoConfig -> AForm WebApp WebApp RepoConfig
+editRepositoryAForm def = RepoConfig
+	<$> areq textField "Description" (Just $ repoDescription def)
+
+getRepoConfig :: UUID -> Annex RepoConfig
+getRepoConfig uuid = RepoConfig
+	<$> (T.pack . fromMaybe "" . M.lookup uuid <$> uuidMap)
 
 getEditRepositoryR :: UUID -> Handler RepHtml
-getEditRepositoryR u = error "TODO"
+getEditRepositoryR uuid = bootstrap (Just Config) $ do
+	sideBarDisplay
+	setTitle "Configure repository"
+	
+	curr <- lift $ runAnnex undefined $ getRepoConfig uuid
+	((result, form), enctype) <- lift $
+		runFormGet $ renderBootstrap $ editRepositoryAForm curr
+	case result of
+		FormSuccess input -> do
+			error (show input)
+		_ -> showform form enctype
+	where
+		showform form enctype = do
+			let authtoken = webAppFormAuthToken
+			description <- lift $
+				runAnnex T.empty $  T.pack . concat <$>
+					Remote.prettyListUUIDs [uuid]
+			$(widgetFile "configurators/editrepository")
