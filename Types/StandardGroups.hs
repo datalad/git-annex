79f10b75c16f0fda9bbff2a286ffdf229c755ecc
Joey Hess <joey@kitenet.net>
2013-03-16 11:17:53 -0400

    Add incrementalbackup repository group.

--- Types/StandardGroups.hs
+++ Types/StandardGroups.hs
@@ -21,6 +21,7 @@ fromStandardGroup :: StandardGroup -> String
 fromStandardGroup ClientGroup = "client"
 fromStandardGroup TransferGroup = "transfer"
 fromStandardGroup BackupGroup = "backup"
+fromStandardGroup IncrementalBackupGroup = "incrementalbackup"
 fromStandardGroup SmallArchiveGroup = "smallarchive"
 fromStandardGroup FullArchiveGroup = "archive"
 fromStandardGroup SourceGroup = "source"
@@ -30,6 +31,7 @@ toStandardGroup :: String -> Maybe StandardGroup
 toStandardGroup "client" = Just ClientGroup
 toStandardGroup "transfer" = Just TransferGroup
 toStandardGroup "backup" = Just BackupGroup
+toStandardGroup "incrementalbackup" = Just IncrementalBackupGroup
 toStandardGroup "smallarchive" = Just SmallArchiveGroup
 toStandardGroup "archive" = Just FullArchiveGroup
 toStandardGroup "source" = Just SourceGroup
@@ -39,7 +41,8 @@ toStandardGroup _ = Nothing
 descStandardGroup :: StandardGroup -> String
 descStandardGroup ClientGroup = "client: a repository on your computer"
 descStandardGroup TransferGroup = "transfer: distributes files to clients"
-descStandardGroup BackupGroup = "backup: backs up all files"
+descStandardGroup BackupGroup = "full backup: backs up all files"
+descStandardGroup IncrementalBackupGroup = "incremental backup: backs up files not backed up elsewhere"
 descStandardGroup SmallArchiveGroup = "small archive: archives files located in \"archive\" directories"
 descStandardGroup FullArchiveGroup = "full archive: archives all files not archived elsewhere"
 descStandardGroup SourceGroup = "file source: moves files on to other repositories"
@@ -50,6 +53,7 @@ preferredContent :: StandardGroup -> String
 preferredContent ClientGroup = "exclude=*/archive/* and exclude=archive/*"
 preferredContent TransferGroup = "not (inallgroup=client and copies=client:2) and " ++ preferredContent ClientGroup
 preferredContent BackupGroup = "include=*"
+preferredContent IncrementalBackupGroup = "include=* and (not copies=incrementalbackup:1)"
 preferredContent SmallArchiveGroup = "(include=*/archive/* or include=archive/*) and " ++ preferredContent FullArchiveGroup
 preferredContent FullArchiveGroup = "not (copies=archive:1 or copies=smallarchive:1)"
 preferredContent SourceGroup = "not (copies=1)"
