e62de3f3b82e361a3ef4cddf2b7181eba5ab41e6
Joey Hess <joey@kitenet.net>
2013-04-17 12:02:44 -0400

    split out library

--- Setup.hs
+++ Setup.hs
@@ -13,7 +13,7 @@ import Control.Applicative
 import Control.Monad
 import System.Directory
 
-import qualified Build.InstallDesktopFile as InstallDesktopFile
+import qualified Build.DesktopFile as DesktopFile
 import qualified Build.Configure as Configure
 
 main = defaultMainWithHooks simpleUserHooks
@@ -58,6 +58,6 @@ installManpages copyDest verbosity pkg lbi =
 
 installDesktopFile :: CopyDest -> Verbosity -> PackageDescription -> LocalBuildInfo -> IO ()
 installDesktopFile copyDest verbosity pkg lbi =
-	InstallDesktopFile.install $ dstBinDir </> "git-annex"
+	DesktopFile.install $ dstBinDir </> "git-annex"
   where
 	dstBinDir = bindir $ absoluteInstallDirs pkg lbi copyDest
