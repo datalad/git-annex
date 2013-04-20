580f22f45db4c5b0f7f71c214b72ee5b0f152f99
Joey Hess <joey@kitenet.net>
2013-04-20 19:27:36 -0400

    forgot to commit this new file

---
+++ Assistant/Install/Menu.hs
@@ -0,0 +1,28 @@
+{- Assistant menu installation.
+ -
+ - Copyright 2013 Joey Hess <joey@kitenet.net>
+ -
+ - Licensed under the GNU GPL version 3 or higher.
+ -}
+
+{-# LANGUAGE CPP #-}
+
+module Assistant.Install.Menu where
+
+import Utility.FreeDesktop
+
+installMenu :: FilePath -> FilePath -> IO ()
+installMenu command file = do
+#ifndef darwin_HOST_OS
+	writeDesktopMenuFile (fdoDesktopMenu command) file
+#endif
+
+{- The command can be either just "git-annex", or the full path to use
+ - to run it. -}
+fdoDesktopMenu :: FilePath -> DesktopEntry
+fdoDesktopMenu command = genDesktopEntry
+	"Git Annex"
+	"Track and sync the files in your Git Annex"
+	False
+	(command ++ " webapp")
+	["Network", "FileTransfer"]
