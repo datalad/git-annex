8661dba58774329baa412b9a4fe27c04218bf275
Joey Hess <joey@kitenet.net>
2013-07-09 14:46:20 -0400

    adjust test suite for changed dropunused behavior with bogus numbers

--- Test.hs
+++ Test.hs
@@ -582,9 +582,9 @@ test_unused env = "git-annex unused/dropunused" ~: intmpclonerepoInDirect env $ 
 		@? "dropkey failed"
 	checkunused [sha1annexedfilekey] ("after dropkey --force " ++ Types.Key.key2file annexedfilekey)
 
-	git_annex env "dropunused" ["1", "2"] @? "dropunused failed"
+	git_annex env "dropunused" ["1"] @? "dropunused failed"
 	checkunused [] "after dropunused"
-	git_annex env "dropunused" ["10", "501"] @? "dropunused failed on bogus numbers"
+	not <$> git_annex env "dropunused" ["10", "501"] @? "dropunused failed to fail on bogus numbers"
 
   where
 	checkunused expectedkeys desc = do
