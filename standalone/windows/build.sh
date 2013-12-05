fee598ad43518071cb3c9b848d87d122649c97b4
Joey Hess <joey@kitenet.net>
2013-12-05 17:56:25 -0400

    nuke ghc too

--- standalone/windows/build.sh
+++ standalone/windows/build.sh
@@ -6,7 +6,7 @@
 set -x
 set -e
 
-mv /c/Users/jenkins/AppData/Roaming/cabal /c/Users/jenkins/AppData/Roaming/cabal.old
+rm -f /c/Users/jenkins/AppData/Roaming/cabal /c/Users/jenkins/AppData/Roaming/ghc
 
 HP="/c/Program Files (x86)/Haskell Platform/2012.4.0.0"
 
