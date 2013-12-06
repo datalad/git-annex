fee56536dc743f21828959268dfc7115cdebc3cb
Joey Hess <joey@kitenet.net>
2013-12-06 17:38:41 -0400

    more verbose cabal

--- Build/EvilLinker.hs
+++ Build/EvilLinker.hs
@@ -134,7 +134,7 @@ runAtFile p s f extraparams = do
 
 main = do
 	ghcout <- fst <$> getOutput "cabal"
-		["build", "--ghc-options=-v -keep-tmp-files"] Nothing
+		["build", "-v2", "--ghc-options=-v -keep-tmp-files"] Nothing
 	gccout <- fst <$> runAtFile parseGhcLink ghcout "gcc.opt" ["-v"]
 	collect2out <- fst <$> runAtFile parseGccLink gccout "collect2.opt" ["-v"]
 	(out, ok) <- runAtFile parseCollect2 collect2out "ld.opt" []
