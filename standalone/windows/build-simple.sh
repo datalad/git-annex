7abdc55dfdc6c3ca6237d92e12ecd7fd9e2aacec
Joey Hess <joey@kitenet.net>
2013-12-05 21:02:27 -0400

    fixes

--- standalone/windows/build-simple.sh
+++ standalone/windows/build-simple.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 # Script to build git-annex on windows. Run by build.bat
+# Does not currently build the NSIS installer. See build.sh for how to do
+# that.
 
 set -e
+set -x
 
 # Path to the Haskell Platform.
-HP="/c/Program Files (x86)/Haskell Platform/2012.4.0.0"
+HP="/c/Program Files/Haskell Platform/2013.2.0.0"
 
-PATH="$HP/bin:$HP/lib/extralibs/bin:/c/Program Files (x86)/NSIS:$PATH"
+PATH="$HP/bin:$HP/lib/extralibs/bin:$PATH"
 
 # Run a command with the cygwin environment available.
 # However, programs not from cygwin are preferred.
@@ -25,7 +28,7 @@ withcyg cabal build || true
 
 # Works around link failure https://ghc.haskell.org/trac/ghc/ticket/8596
 # using a response file.
-rm -f build.log gcc.opt
+withcyg rm -f build.log gcc.opt
 withcyg cabal build --ghc-options='-v -keep-tmp-files' > build.log 2>&1
-grep '"dist\\build\\git-annex\\git-annex.exe"' build.log | sed -e 's/^"[^"]*" //' -e 's/\\/\//g' > gcc.opt
+withcyg grep '"dist\\build\\git-annex\\git-annex.exe"' build.log | sed -e 's/^"[^"]*" //' -e 's/\\/\//g' > gcc.opt
 "$HP/mingw/bin/gcc.exe" @gcc.opt
