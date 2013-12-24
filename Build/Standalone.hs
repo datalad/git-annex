e199240ab8a462366274296e8fad8ddd298c61cd
Joey Hess <joey@kitenet.net>
2013-12-24 15:18:15 -0400

    remove unused imports

--- Build/Standalone.hs
+++ Build/Standalone.hs
@@ -9,21 +9,13 @@
 
 module Main where
 
-import Control.Applicative
 import Control.Monad.IfElse
 import System.Environment
-import Data.Maybe
 import System.FilePath
 import System.Directory
-import System.IO
 import Control.Monad
-import Data.List
 import Build.BundledPrograms
 
-import Utility.PartialPrelude
-import Utility.Directory
-import Utility.Process
-import Utility.Monad
 import Utility.SafeCommand
 import Utility.Path
 
