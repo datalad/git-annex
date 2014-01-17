0d544649d0039e66fe9128ac8e5ef7fe6e1cd538
Joey Hess <joey@kitenet.net>
2014-01-16 21:24:17 -0400

    catch exception checking if url exists when network is disconnected
    Leads to better failure message (or possibly fallback to another remote).

--- Remote/Web.hs
+++ Remote/Web.hs
@@ -118,7 +118,8 @@ checkKey' key us = firsthit us (Right False) $ \u -> do
 #endif
 		DefaultDownloader -> do
 			headers <- getHttpHeaders
-			Right <$> Url.withUserAgent (Url.checkBoth u' headers $ keySize key)
+			Url.withUserAgent $ catchMsgIO .
+				Url.checkBoth u' headers (keySize key)
   where
   	firsthit [] miss _ = return miss
 	firsthit (u:rest) _ a = do
