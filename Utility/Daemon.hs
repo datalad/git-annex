1ad3c1b2b30eb93fc34113a06af74521bc7322ac
Joey Hess <joey@kitenet.net>
2014-01-29 15:24:22 -0400

    fix windows build

--- Utility/Daemon.hs
+++ Utility/Daemon.hs
@@ -18,7 +18,7 @@ import Utility.LogFile
 import System.Posix
 import Control.Concurrent.Async
 #else
-import System.PosixCompat
+import System.PosixCompat.Types
 #endif
 
 {- Run an action as a daemon, with all output sent to a file descriptor.
