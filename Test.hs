datalad/git-annex

commit 1c997fd08c04b27c9acb561a33c6fb2829089be7

fix test suite to pass on windows
On windows, the sync of the second cloned repo to origin failed, because
synced/master was a non-fast-forward. This may be a bug of its own, but
it's not the issue that this test was intended to test, so disconnect
the repos from origin before syncing.

Test.hs
@@ -781,6 +781,7 @@ test_conflict_resolution_movein_bug env = withtmpclonerepo env False $ \r1 -> do
 		forM_ [r1, r2] $ \r -> indir env r $ do
 			{- Get all files, see check below. -}
 			git_annex env "get" [] @? "get failed"
+			disconnectOrigin
 		pair env r1 r2
 		forM_ [r1, r2] $ \r -> indir env r $ do
 			{- Set up a conflict. -}
@@ -815,10 +816,12 @@ test_mixed_conflict_resolution env = do
 	check_mixed_conflict inr1 = withtmpclonerepo env False $ \r1 ->
 		withtmpclonerepo env False $ \r2 -> do
 			indir env r1 $ do
+				disconnectOrigin
 				writeFile conflictor "conflictor"
 				git_annex env "add" [conflictor] @? "add conflicter failed"
 				git_annex env "sync" [] @? "sync failed in r1"
 			indir env r2 $ do
+				disconnectOrigin
 				createDirectory conflictor
 				writeFile (conflictor </> "subfile") "subfile"
 				git_annex env "add" [conflictor] @? "add conflicter failed"
@@ -838,16 +841,13 @@ test_mixed_conflict_resolution env = do
 				<$> getDirectoryContents d)
 				@? (d ++ "conflictor file missing")
 
-{- Set up repos as remotes of each other;
- - remove origin since we're going to sync
- - some changes to a file. -}
+{- Set up repos as remotes of each other. -}
 pair :: TestEnv -> FilePath -> FilePath -> Assertion
 pair env r1 r2 = forM_ [r1, r2] $ \r -> indir env r $ do
 	when (r /= r1) $
 		boolSystem "git" [Params "remote add r1", File ("../../" ++ r1)] @? "remote add"
 	when (r /= r2) $
 		boolSystem "git" [Params "remote add r2", File ("../../" ++ r2)] @? "remote add"
-	boolSystem "git" [Params "remote rm origin"] @? "remote rm"
 
 test_map :: TestEnv -> Assertion
 test_map env = intmpclonerepo env $ do
@@ -1116,6 +1116,9 @@ withtmpclonerepo :: TestEnv -> Bool -> (FilePath -> Assertion) -> Assertion
 withtmpclonerepo env bare a = do
 	dir <- tmprepodir
 	bracket (clonerepo env mainrepodir dir bare) cleanup a
+
+disconnectOrigin :: Assertion
+disconnectOrigin = boolSystem "git" [Params "remote rm origin"] @? "remote rm"
 
 withgitrepo :: TestEnv -> (FilePath -> Assertion) -> Assertion
 withgitrepo env = bracket (setuprepo env mainrepodir) return

doc/bugs/test_failures_on_window_for_5.20131118.mdwn
@@ -19,6 +19,4 @@ windows 7, NTFS = 2 FAILs
 
 see attachment for full log of git annex test output
 
-> I am seeing the same 2 failures now (I used to see a lot more!)
-> and have put some analysis in [[todo/windows_support]] about them.
-> --[[Joey]]
+> Reproduced and [[fixed|done]]. --[[Joey]]

doc/todo/windows_support.mdwn
@@ -12,9 +12,6 @@ now! --[[Joey]]
   is much slower than on systems supporting connection caching.
 * The test suite does not 100% pass, although it seems to be getting
   quite close! Current known failures:
-  * "conflict_resolution (mixed directory and file)" test fails;
-    not because the conflict resolution fails, but the sync fails
-    pushing to synced/master with a non-fast-forward failure.
   * Some tests fail if git has not had the username and email configured.
     This is odd because the test suite goes to some lengths to
     get env vars set to prevent this problem.

commit 617c63a213c8bf5c827a92a655427ed93523a574

fix broken test
1c997fd08c04b27c9acb561a33c6fb2829089be7 broke the mixed conflict
resolution test. Since origin is no longer involved, both sides have to
sync after pairing. (The sync before pairing is just to get a commit.)

Test.hs
@@ -827,19 +827,19 @@ test_mixed_conflict_resolution env = do
 				git_annex env "add" [conflictor] @? "add conflicter failed"
 				git_annex env "sync" [] @? "sync failed in r2"
 			pair env r1 r2
-			let r = if inr1 then r1 else r2
-			indir env r $ do
+			let l = if inr1 then [r1, r2] else [r2, r1]
+			forM_ l $ \r -> indir env r $
 				git_annex env "sync" [] @? "sync failed in mixed conflict"
-			checkmerge r1
-			checkmerge r2
+			checkmerge "r1" r1
+			checkmerge "r1" r2
 	  where
 		conflictor = "conflictor"
 		variantprefix = conflictor ++ ".variant"
-		checkmerge d = do
+		checkmerge what d = do
 			doesDirectoryExist (d </> conflictor) @? (d ++ " conflictor directory missing")
 			l <- getDirectoryContents d
 			any (variantprefix `isPrefixOf`) l
-				@? (d ++ " conflictor file missing in: " ++ show l )
+				@? (what ++ " conflictor file missing in: " ++ show l )
 
 {- Set up repos as remotes of each other. -}
 pair :: TestEnv -> FilePath -> FilePath -> Assertion