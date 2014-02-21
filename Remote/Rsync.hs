datalad/git-annex

commit a1eded86413ede5d5996a5f635e5f05cbefaa5a2

Allow rsync to use other remote shells.
Introduced a new per-remote option 'annex-rsync-transport' to specify
the remote shell that it to be used with rsync. In case the value is
'ssh', connections are cached unless 'sshcaching' is unset.

Remote/Rsync.hs
@@ -17,6 +17,7 @@ import qualified Git
 import Config
 import Config.Cost
 import Annex.Content
+import Annex.Ssh
 import Remote.Helper.Special
 import Remote.Helper.Encryptable
 import Crypto
@@ -44,6 +45,9 @@ remote = RemoteType {
 gen :: Git.Repo -> UUID -> RemoteConfig -> RemoteGitConfig -> Annex Remote
 gen r u c gc = do
 	cst <- remoteCost gc expensiveRemoteCost
+	(transport, url) <- rsyncTransport
+	let o = RsyncOpts url (transport ++ opts) escape
+	    islocal = rsyncUrlIsPath $ rsyncUrl o
 	return $ encryptableRemote c
 		(storeEncrypted o $ getGpgOpts gc)
 		(retrieveEncrypted o)
@@ -69,9 +73,6 @@ gen r u c gc = do
 			, remotetype = remote
 			}
   where
-	o = RsyncOpts url opts escape
-	islocal = rsyncUrlIsPath $ rsyncUrl o
-	url = fromMaybe (error "missing rsyncurl") $ remoteAnnexRsyncUrl gc
 	opts = map Param $ filter safe $ remoteAnnexRsyncOptions gc
 	escape = M.lookup "shellescape" c /= Just "no"
 	safe opt
@@ -81,6 +82,28 @@ gen r u c gc = do
 		| opt == "--delete" = False
 		| opt == "--delete-excluded" = False
 		| otherwise = True
+	rawurl = fromMaybe (error "missing rsyncurl") $ remoteAnnexRsyncUrl gc
+	(login,resturl) = case separate (=='@') rawurl of
+			(h, "") -> (Nothing, h)
+			(l, h)  -> (Just l, h)
+	loginopt = maybe [] (\l -> ["-l",l]) login
+	fromNull as xs | null xs = as
+		       | otherwise = xs
+	rsyncTransport = if rsyncUrlIsShell rawurl
+		then (\rsh -> return (rsyncShell rsh, resturl)) =<<
+			case fromNull ["ssh"] (remoteAnnexRsyncTransport gc) of
+				"ssh":sshopts -> do
+					let (port, sshopts') = sshReadPort sshopts
+					    host = takeWhile (/=':') resturl
+					-- Connection caching
+					(Param "ssh":) <$> sshCachingOptions
+						(host, port)
+						(map Param $ loginopt ++ sshopts')
+				"rsh":rshopts -> return $ map Param $ "rsh" :
+					loginopt ++ rshopts
+				rsh -> error $ "Unknown Rsync transport: "
+					++ unwords rsh
+		else return ([], rawurl)
 
 rsyncSetup :: UUID -> RemoteConfig -> Annex RemoteConfig
 rsyncSetup u c = do

Annex/Ssh.hs
@@ -8,8 +8,9 @@
 {-# LANGUAGE CPP #-}
 
 module Annex.Ssh (
-	sshParams,
+	sshCachingOptions,
 	sshCleanup,
+	sshReadPort,
 ) where
 
 import qualified Data.Map as M
@@ -24,8 +25,8 @@ import Config
 
 {- Generates parameters to ssh to a given host (or user@host) on a given
  - port, with connection caching. -}
-sshParams :: (String, Maybe Integer) -> [CommandParam] -> Annex [CommandParam]
-sshParams (host, port) opts = go =<< sshInfo (host, port)
+sshCachingOptions :: (String, Maybe Integer) -> [CommandParam] -> Annex [CommandParam]
+sshCachingOptions (host, port) opts = go =<< sshInfo (host, port)
   where
 	go (Nothing, params) = ret params
 	go (Just socketfile, params) = do
@@ -33,8 +34,7 @@ sshParams (host, port) opts = go =<< sshInfo (host, port)
 		liftIO $ createDirectoryIfMissing True $ parentDir socketfile
 		lockFile $ socket2lock socketfile
 		ret params
-	ret ps = return $ ps ++ opts ++ portParams port ++
-		[Param "-T", Param host]
+	ret ps = return $ ps ++ opts ++ portParams port ++ [Param "-T"]
 	-- If the lock pool is empty, this is the first ssh of this
 	-- run. There could be stale ssh connections hanging around
 	-- from a previous git-annex run that was interrupted.
@@ -154,3 +154,15 @@ sizeof_sockaddr_un_sun_path = 100
  - appear on disk. -}
 valid_unix_socket_path :: FilePath -> Bool
 valid_unix_socket_path f = length (decodeW8 f) < sizeof_sockaddr_un_sun_path
+
+{- Parses the SSH port, and returns the other OpenSSH options. If
+ - several ports are found, the last one takes precedence. -}
+sshReadPort :: [String] -> (Maybe Integer, [String])
+sshReadPort params = (port, reverse args)
+  where
+	(port,args) = aux (Nothing, []) params
+	aux (p,ps) [] = (p,ps)
+	aux (_,ps) ("-p":p:rest) = aux (readPort p, ps) rest
+	aux (p,ps) (q:rest) | "-p" `isPrefixOf` q = aux (readPort $ drop 2 q, ps) rest
+			    | otherwise = aux (p,q:ps) rest
+	readPort p = fmap fst $ listToMaybe $ reads p

Remote/Helper/Ssh.hs
@@ -22,9 +22,10 @@ sshToRepo :: Git.Repo -> [CommandParam] -> Annex [CommandParam]
 sshToRepo repo sshcmd = do
 	g <- fromRepo id
 	let c = extractRemoteGitConfig g (Git.repoDescribe repo)
-	let opts = map Param $ remoteAnnexSshOptions c
-	params <- sshParams (Git.Url.hostuser repo, Git.Url.port repo) opts
-	return $ params ++ sshcmd
+	    opts = map Param $ remoteAnnexSshOptions c
+            host = Git.Url.hostuser repo
+	params <- sshCachingOptions (host, Git.Url.port repo) opts
+	return $ params ++ Param host : sshcmd
 
 {- Generates parameters to run a git-annex-shell command on a remote
  - repository. -}

Types/GitConfig.hs
@@ -94,6 +94,7 @@ data RemoteGitConfig = RemoteGitConfig
 	 - including special remotes. -}
 	, remoteAnnexSshOptions :: [String]
 	, remoteAnnexRsyncOptions :: [String]
+	, remoteAnnexRsyncTransport :: [String]
 	, remoteAnnexGnupgOptions :: [String]
 	, remoteAnnexRsyncUrl :: Maybe String
 	, remoteAnnexBupRepo :: Maybe String
@@ -116,6 +117,7 @@ extractRemoteGitConfig r remotename = RemoteGitConfig
 
 	, remoteAnnexSshOptions = getoptions "ssh-options"
 	, remoteAnnexRsyncOptions = getoptions "rsync-options"
+	, remoteAnnexRsyncTransport = getoptions "rsync-transport"
 	, remoteAnnexGnupgOptions = getoptions "gnupg-options"
 	, remoteAnnexRsyncUrl = notempty $ getmaybe "rsyncurl"
 	, remoteAnnexBupRepo = getmaybe "buprepo"

doc/git-annex.mdwn
@@ -943,6 +943,15 @@ Here are all the supported configuration settings.
   to or from this remote. For example, to force ipv6, and limit
   the bandwidth to 100Kbyte/s, set it to "-6 --bwlimit 100"
 
+* `remote.<name>.annex-rsync-transport`
+
+  The remote shell to use to connect to the rsync remote. Possible
+  values are `ssh` (the default) and `rsh`, together with their
+  arguments, for instance `ssh -p 2222 -c blowfish`; Note that the
+  remote hostname should not appear there, see rsync(1) for details.
+  When the transport used is `ssh`, connections are automatically cached
+  unless `annex.sshcaching` is unset.
+
 * `remote.<name>.annex-bup-split-options`
 
   Options to pass to bup split when storing content in this remote.

commit ded4ab5704371dc7452c58fe11b3151d5cd2b68d

Fix handling of rsync remote urls containing a username, including rsync.net.
This breakage seems to have been caused way back in a1eded86,
but I am pretty sure rsync.net support has not been entirely
broken since last April. AFAICS, the generated .ssh/config
has not changed since then -- it has never included a Username setting
line. So, I am puzzled at when this reversion was introduced.

Note that the breakage only affected checkpresent and remove. Upload and
download use the ssh connection caching, which includes a -l username.

Remote/Rsync.hs
@@ -112,26 +112,26 @@ genRsyncOpts c gc transport url = RsyncOpts
 		| otherwise = True
 
 rsyncTransport :: RemoteGitConfig -> RsyncUrl -> Annex ([CommandParam], RsyncUrl)
-rsyncTransport gc rawurl
-	| rsyncUrlIsShell rawurl =
-		(\rsh -> return (rsyncShell rsh, resturl)) =<<
+rsyncTransport gc url
+	| rsyncUrlIsShell url =
+		(\rsh -> return (rsyncShell rsh, url)) =<<
 		case fromNull ["ssh"] (remoteAnnexRsyncTransport gc) of
 			"ssh":sshopts -> do
 				let (port, sshopts') = sshReadPort sshopts
-				    host = takeWhile (/=':') resturl
+				    userhost = takeWhile (/=':') url
 				-- Connection caching
 				(Param "ssh":) <$> sshCachingOptions
-					(host, port)
+					(userhost, port)
 					(map Param $ loginopt ++ sshopts')
 			"rsh":rshopts -> return $ map Param $ "rsh" :
 				loginopt ++ rshopts
 			rsh -> error $ "Unknown Rsync transport: "
 				++ unwords rsh
-	| otherwise = return ([], rawurl)
+	| otherwise = return ([], url)
   where
-	(login,resturl) = case separate (=='@') rawurl of
-		(h, "") -> (Nothing, h)
-		(l, h)  -> (Just l, h)
+	login = case separate (=='@') url of
+		(_h, "") -> Nothing
+		(l, _)  -> Just l
 	loginopt = maybe [] (\l -> ["-l",l]) login
 	fromNull as xs = if null xs then as else xs
 

doc/bugs/rsync_transport:_username_not_respected.mdwn
@@ -36,3 +36,8 @@ Type: prebuilt
 
 # End of transcript or log.
 """]]
+
+> Argh! How did that break? I know it used to work.
+> I have fixed it, unfortunately the fix was too late for today's release,
+> but it will be available in autobuilds shortly.
+> [[fixed|done]] --[[Joey]] 