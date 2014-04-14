e53a85743e4888dd60d7d9a7fa09e95c06b26bd6
Joey Hess <joey@kitenet.net>
2014-04-14 13:37:12 -0400

    adjust to not use cpp in modules used by configure

--- Utility/Network.hs
+++ Utility/Network.hs
@@ -5,22 +5,12 @@
  - Licensed under the GNU GPL version 3 or higher.
  -}
 
-{-# LANGUAGE CPP #-}
-
 module Utility.Network where
 
 import Utility.Process
 import Utility.Exception
 
 import Control.Applicative
-
--- Old versions of network lacked an Ord for URI
-#if ! MIN_VERSION_network(2,4,0)
-import Network.URI
-
-instance Ord URI where
-	a `compare` b = show a `compare` show b
-#endif
 
 {- Haskell lacks uname(2) bindings, except in the
  - Bindings.Uname addon. Rather than depend on that,
