8ad5df7aa3aeade681d1697a10220f366d16e375
Joey Hess <joeyh@joeyh.name>
2015-01-08 19:39:04 -0400

    fix OS build

--- Assistant/Install/AutoStart.hs
+++ Assistant/Install/AutoStart.hs
@@ -14,7 +14,7 @@ import Utility.FreeDesktop
 import Utility.OSX
 import Utility.Path
 import System.Directory
-import System.Path
+import System.FilePath
 #endif
 
 installAutoStart :: FilePath -> FilePath -> IO ()
