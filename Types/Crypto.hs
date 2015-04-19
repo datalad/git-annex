be3895be45c61d0c240f3f1ca1882b6f84e3c89c
Joey Hess <joeyh@joeyh.name>
2015-04-19 10:57:14 -0400

    refactor

--- Types/Crypto.hs
+++ Types/Crypto.hs
@@ -17,12 +17,7 @@ module Types.Crypto (
 	calcMac,
 ) where
 
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as T
-import Data.Digest.Pure.SHA
-import qualified Data.ByteString as B
-import Crypto.Hash
-
+import Utility.Hash
 import Utility.Gpg (KeyIds(..))
 
 -- XXX ideally, this would be a locked memory region
@@ -33,13 +28,6 @@ data StorableCipher = EncryptedCipher String EncryptedCipherVariant KeyIds
 	deriving (Ord, Eq)
 data EncryptedCipherVariant = Hybrid | PubKey
 	deriving (Ord, Eq)
-
-{- File names are (client-side) MAC'ed on special remotes.
- - The chosen MAC algorithm needs to be same for all files stored on the
- - remote.
- -}
-data Mac = HmacSha1 | HmacSha224 | HmacSha256 | HmacSha384 | HmacSha512
-	deriving (Eq)
 
 defaultMac :: Mac
 defaultMac = HmacSha1
@@ -60,30 +48,3 @@ readMac "HMACSHA256" = Just HmacSha256
 readMac "HMACSHA384" = Just HmacSha384
 readMac "HMACSHA512" = Just HmacSha512
 readMac _ = Nothing
-
-calcMac
-	:: Mac          -- ^ MAC
-	-> B.ByteString -- ^ secret key
-	-> B.ByteString -- ^ message
-	-> String       -- ^ MAC'ed message, in hexadecimal
-calcMac mac = case mac of
-	HmacSha1   -> use SHA1
-	HmacSha224 -> use SHA224
-	HmacSha256 -> use SHA256
-	HmacSha384 -> use SHA384
-	HmacSha512 -> use SHA512
-  where
-	use alg k m = show (hmacGetDigest (hmacAlg alg k m))
-
--- Check that all the MACs continue to produce the same.
-prop_mac_stable :: Bool
-prop_mac_stable = all (\(mac, result) -> calcMac mac key msg == result)
-	[ (HmacSha1, "46b4ec586117154dacd49d664e5d63fdc88efb51")
-	, (HmacSha224, "4c1f774863acb63b7f6e9daa9b5c543fa0d5eccf61e3ffc3698eacdd")
-	, (HmacSha256, "f9320baf0249169e73850cd6156ded0106e2bb6ad8cab01b7bbbebe6d1065317")
-	, (HmacSha384, "3d10d391bee2364df2c55cf605759373e1b5a4ca9355d8f3fe42970471eca2e422a79271a0e857a69923839015877fc6")
-	, (HmacSha512, "114682914c5d017dfe59fdc804118b56a3a652a0b8870759cf9e792ed7426b08197076bf7d01640b1b0684df79e4b67e37485669e8ce98dbab60445f0db94fce")
-	]
-  where
-	key = T.encodeUtf8 $ T.pack "foo"
-	msg = T.encodeUtf8 $ T.pack "bar"
