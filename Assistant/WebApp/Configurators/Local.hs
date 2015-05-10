9ee2745ca5a5d6655788547721a4a8c46ab30445
Joey Hess <joeyh@joeyh.name>
2015-05-10 15:56:07 -0400

    typo

--- Assistant/WebApp/Configurators/Local.hs
+++ Assistant/WebApp/Configurators/Local.hs
@@ -5,7 +5,7 @@
  - Licensed under the GNU AGPL version 3 or higher.
  -}
 
-{-# LANGUAGE CPP, QuasiQuotes, TemplateHaskell, OverloadedStrings, #-}
+{-# LANGUAGE CPP, QuasiQuotes, TemplateHaskell, OverloadedStrings #-}
 {-# LANGUAGE RankNTypes, KindSignatures, TypeFamilies, FlexibleContexts #-}
 
 module Assistant.WebApp.Configurators.Local where
