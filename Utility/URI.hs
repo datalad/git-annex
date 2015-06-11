256b86b9486f7f10f8c4811f495d75c5cbed5ec9
Joey Hess <joeyh@joeyh.name>
2015-06-11 16:43:59 -0400

    oh foo, I didn't mean to include this in the prev commit

--- Utility/URI.hs
+++ Utility/URI.hs
@@ -8,3 +8,11 @@
 {-# LANGUAGE CPP #-}
 
 module Utility.URI where
+
+-- Old versions of network lacked an Ord for URI
+#if ! MIN_VERSION_network(2,4,0)
+import Network.URI
+
+instance Ord URI where
+	a `compare` b = show a `compare` show b
+#endif
