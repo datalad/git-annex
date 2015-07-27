52ecf232fb543ba69674e80f50243bf67aa50287
Joey Hess <joeyh@joeyh.name>
2015-07-27 11:36:39 -0400

    only push when needsinit

--- Assistant/WebApp/Configurators/Ssh.hs
+++ Assistant/WebApp/Configurators/Ssh.hs
@@ -506,11 +506,12 @@ prepSsh' needsinit origsshdata sshdata keypair a
 		{- To ensure the repository is initialized, try to push the
 		 - git-annex branch to it. Then git-annex-shell will see
 		 - the branch and auto-initialize. -}
-		void $ liftAnnex $ inRepo $ Git.Command.runBool
-			[ Param "push"
-			, Param (genSshUrl sshdata)
-			, Param (fromRef Annex.Branch.name)
-			]
+		when needsinit $ do
+			void $ liftAnnex $ inRepo $ Git.Command.runBool
+				[ Param "push"
+				, Param (genSshUrl sshdata)
+				, Param (fromRef Annex.Branch.name)
+				]
 		a sshdata
 	| otherwise = sshSetup (mkSshInput origsshdata)
 		 [ "-p", show (sshPort origsshdata)
