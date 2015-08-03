6ca08f02a40a0728d135e2ffaf9f74671927426f
Joey Hess <joeyh@joeyh.name>
2015-08-03 15:49:35 -0400

    remove unused imports

--- Utility/LockPool/Windows.hs
+++ Utility/LockPool/Windows.hs
@@ -16,14 +16,10 @@ module Utility.LockPool.Windows (
 import qualified Utility.LockFile.Windows as F
 import qualified Utility.LockPool.STM as P
 import Utility.LockPool.LockHandle
-import Utility.LockPool.STM (LockPool, LockFile, LockMode(..))
-import Utility.Monad
+import Utility.LockPool.STM (LockFile, LockMode(..))
 
-import Control.Concurrent.STM
 import System.IO
 import Data.Maybe
-import Control.Applicative
-import Prelude
 
 {- Tries to lock a file with a shared lock, which allows other processes to
  - also lock it shared. Fails if the file is exclusively locked. -}
