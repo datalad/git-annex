datalad/git-annex

commit 23e9d3bb777d580931a5b4d7ee57aa604190d8b2

Fix setting/setting/viewing metadata that contains unicode or other special characters, when in a non-unicode locale.
Oh boy, not again. So, another place that the filesystem encoding needs to
be applied. Yay.

In passing, I changed decodeBS so if a NUL is embedded in the input, the
resulting FilePath doesn't get truncated at that NUL. This was needed to
make prop_b64_roundtrips pass, and on reviewing the callers of decodeBS, I
didn't see any where this wouldn't make sense. When a FilePath is used to
operate on the filesystem, it'll get truncated at a NUL anyway, whereas if
a String is being used for something else, it might conceivably have a NUL
in it, and we wouldn't want it to get truncated when going through
decodeBS.
(NB: There may be a speed impact from this change.)

Utility/Base64.hs
@@ -1,5 +1,8 @@
 {- Simple Base64 encoding of Strings
  -
+ - Note that this uses the FileSystemEncoding, so it can be used on Strings
+ - that repesent filepaths containing arbitrarily encoded characters.
+ -
  - Copyright 2011, 2015 Joey Hess <id@joeyh.name>
  -
  - License: BSD-2-clause
@@ -9,13 +12,15 @@ module Utility.Base64 (toB64, fromB64Maybe, fromB64, prop_b64_roundtrips) where
 
 import qualified "sandi" Codec.Binary.Base64 as B64
 import Data.Maybe
+import qualified Data.ByteString.Lazy as L
 import Data.ByteString.UTF8 (fromString, toString)
+import Utility.FileSystemEncoding
 
 toB64 :: String -> String	
-toB64 = toString . B64.encode . fromString
+toB64 = toString . B64.encode . L.toStrict . encodeBS
 
 fromB64Maybe :: String -> Maybe String
-fromB64Maybe s = either (const Nothing) (Just . toString)
+fromB64Maybe s = either (const Nothing) (Just . decodeBS . L.fromStrict)
 	(B64.decode $ fromString s)
 
 fromB64 :: String -> String

Utility/FileSystemEncoding.hs
@@ -13,6 +13,7 @@ module Utility.FileSystemEncoding (
 	withFilePath,
 	md5FilePath,
 	decodeBS,
+	encodeBS,
 	decodeW8,
 	encodeW8,
 	encodeW8NUL,
@@ -81,11 +82,19 @@ md5FilePath = MD5.Str . _encodeFilePath
 {- Decodes a ByteString into a FilePath, applying the filesystem encoding. -}
 decodeBS :: L.ByteString -> FilePath
 #ifndef mingw32_HOST_OS
-decodeBS = encodeW8 . L.unpack
+decodeBS = encodeW8NUL . L.unpack
 #else
 {- On Windows, we assume that the ByteString is utf-8, since Windows
  - only uses unicode for filenames. -}
 decodeBS = L8.toString
+#endif
+
+{- Encodes a FilePath into a ByteString, applying the filesystem encoding. -}
+encodeBS :: FilePath -> L.ByteString
+#ifndef mingw32_HOST_OS
+encodeBS = L.pack . decodeW8NUL
+#else
+encodeBS = L8.fromString
 #endif
 
 {- Converts a [Word8] to a FilePath, encoding using the filesystem encoding.

Annex/TaggedPush.hs
@@ -45,7 +45,6 @@ fromTaggedBranch b = case split "/" $ Git.fromRef b of
 	("refs":"synced":u:_base) ->
 		Just (toUUID u, Nothing)
 	_ -> Nothing
-  where
 
 taggedPush :: UUID -> Maybe String -> Git.Ref -> Remote -> Git.Repo -> IO Bool
 taggedPush u info branch remote = Git.Command.runBool

doc/bugs/view_fails_with___34__invalid_character__34__.mdwn
@@ -28,3 +28,29 @@ local repository version: 5
 supported repository version: 5
 upgrade supported from repository versions: 0 1 2 4
 """]]
+
+> I'm assuming the setlocale part of this is a misconfigured system locale;
+> as also seen by an arch linux user in
+> <http://git-annex.branchable.com/bugs/cannot_change_locale___40__en__95__US.UTF-8__41__/>
+> 
+> So, disregarding that part of the bug report, we still have the actual
+> failure. 
+> 
+> With LANG=C, setting and getting metadata like "Rondò Veneziano" fails,
+> as does generating views of that metadata.
+> 
+> In all cases, it's an IO encoding failure, "commitBuffer: invalid argument (invalid character)"
+> 
+> This only occurs when there's a space in the metadata; in this case the
+
+> value is base64ed. While the 'ò' comes back out as "\242", which is the right
+> character, it's not encoded using the filesystem encoding. This means that
+> the IO layer can't handle it, when not in a unicode locale. Instead, it
+> needs to come back out as "\56515\56498".
+> 
+> Apparently this is a reversion; it worked in an earlier version of
+> git-annex. Commits such as 9b93278e8abe1163d53fbf56909d0fe6d7de69e9
+> or the conversion to Sandi may have caused the reversion, unsure.
+> 
+> Fix is to apply the filesystem encoding when decoding base64ed values.
+> [[done]] --[[Joey]]

commit 4e4e11849a0d95389de81461ba2f2a4e0245d3b2

fix test suite fail in LANG=C
This was caused by 23e9d3bb777d580931a5b4d7ee57aa604190d8b2

an Arbitrary String is not necessarily encoded using the filesystem
encoding, and in a non-utf8 locale, encodeBS throws an exception on such a
string. All I could think to do is limit test data to ascii.

This shouldn't be a problem in practice, because the all Strings in
git-annex that are not generated by Arbitrary should be loaded in a way
that does apply the filesystem encoding.

Types/MetaData.hs
@@ -270,10 +270,16 @@ instance Arbitrary MetaData where
 		legal k _v = legalField $ fromMetaField k
 
 instance Arbitrary MetaValue where
-	arbitrary = MetaValue <$> arbitrary <*> arbitrary
+	arbitrary = MetaValue 
+		<$> arbitrary
+		-- Avoid non-ascii metavalues because fully arbitrary
+		-- strings may not be encoded using the filesystem
+		-- encoding, which is norally applied to all input.
+		<*> arbitrary `suchThat` all isAscii
 
 instance Arbitrary MetaField where
-	arbitrary = MetaField . CI.mk <$> arbitrary `suchThat` legalField 
+	arbitrary = MetaField . CI.mk 
+		<$> arbitrary `suchThat` legalField 
 
 prop_metadata_sane :: MetaData -> MetaField -> MetaValue -> Bool
 prop_metadata_sane m f v = and

Utility/Base64.hs
@@ -10,11 +10,13 @@
 
 module Utility.Base64 (toB64, fromB64Maybe, fromB64, prop_b64_roundtrips) where
 
+import Utility.FileSystemEncoding
+
 import qualified "sandi" Codec.Binary.Base64 as B64
 import Data.Maybe
 import qualified Data.ByteString.Lazy as L
 import Data.ByteString.UTF8 (fromString, toString)
-import Utility.FileSystemEncoding
+import Data.Char
 
 toB64 :: String -> String	
 toB64 = toString . B64.encode . L.toStrict . encodeBS
@@ -28,5 +30,9 @@ fromB64 = fromMaybe bad . fromB64Maybe
   where
 	bad = error "bad base64 encoded data"
 
+-- Only ascii strings are tested, because an arbitrary string may contain
+-- characters not encoded using the FileSystemEncoding.
 prop_b64_roundtrips :: String -> Bool
-prop_b64_roundtrips s = s == fromB64 (toB64 s)
+prop_b64_roundtrips s
+	| all (isAscii) s = s == fromB64 (toB64 s)
+	| otherwise = True

Utility/FileSystemEncoding.hs
@@ -68,6 +68,10 @@ withFilePath fp f = Encoding.getFileSystemEncoding
  - only allows doing this conversion with CStrings, and the CString buffer
  - is allocated, used, and deallocated within the call, with no side
  - effects.
+ -
+ - If the FilePath contains a value that is not legal in the filesystem
+ - encoding, this may throw an exception. For example, "\226" is not valid
+ - in the C locale, but is in utf locales.
  -}
 {-# NOINLINE _encodeFilePath #-}
 _encodeFilePath :: FilePath -> String