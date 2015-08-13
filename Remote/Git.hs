983a95f021558cfe64100f200b9949de2ff1a3a3
Joey Hess <joeyh@joeyh.name>
2015-08-13 14:20:28 -0400

    Sped up downloads of files from ssh remotes, reducing the non-data-transfer overhead 6x.

--- Remote/Git.hs
+++ Remote/Git.hs
@@ -58,6 +58,7 @@ import Control.Concurrent
 import Control.Concurrent.MSampleVar
 import qualified Data.Map as M
 import Network.URI
+import System.Log.Logger
 
 remote :: RemoteType
 remote = RemoteType {
@@ -445,9 +446,14 @@ copyFromRemote' r key file dest meterupdate
 		let feeder = \n -> do
 			meterupdate n
 			writeSV v (fromBytesProcessed n)
-		let cleanup = do
+
+		-- It can easily take 0.3 seconds to clean up after
+		-- the transferinfo, and all that's involved is shutting
+		-- down the process and associated thread cleanly. So,
+		-- do it in the background.
+		let cleanup = forkIO $ do
 			void $ tryIO $ killThread tid
-			tryNonAsync $
+			void $ tryNonAsync $
 				maybe noop (void . waitForProcess)
 					=<< tryTakeMVar pidv
 		bracketIO noop (const cleanup) (const $ a feeder)
