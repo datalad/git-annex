75f61df323b8ae1454ff592fd3fbadac27570545
Joey Hess <joeyh@joeyh.name>
2016-01-12 13:31:13 -0400

    cleanup

--- Database/Keys.hs
+++ Database/Keys.hs
@@ -5,10 +5,7 @@
  - Licensed under the GNU GPL version 3 or higher.
  -}
 
-{-# LANGUAGE QuasiQuotes, TypeFamilies, TemplateHaskell #-}
-{-# LANGUAGE OverloadedStrings, GADTs, FlexibleContexts #-}
-{-# LANGUAGE MultiParamTypeClasses, GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE RankNTypes, ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Database.Keys (
 	DbHandle,
