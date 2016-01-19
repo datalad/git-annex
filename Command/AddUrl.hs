80d5feefc76537c8ec41dec21ae168361be31229
Joey Hess <joeyh@joeyh.name>
2016-01-19 12:01:00 -0400

    addurl --json: Include field for added key
    (unless the file was added directly to git due to annex.largefiles configuration.)
    
    (Also done by add --json and import --json)

--- Command/AddUrl.hs
+++ Command/AddUrl.hs
@@ -379,6 +379,7 @@ cleanup u url file key mtmp = case mtmp of
 			)
   where
 	go = do
+		maybeShowJSON [("key", key2file key)]
 		when (isJust mtmp) $
 			logStatus key InfoPresent
 		setUrlPresent u key url
