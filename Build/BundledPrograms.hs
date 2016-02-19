398d93d4d2ee62c81b4c6036d77aa201dc915bb5
Joey Hess <joeyh@joeyh.name>
2016-02-19 16:19:19 -0400

    Linux and OSX standalone builds put the bundled gpg last in PATH, so any system gpg will be preferred over it.

--- Build/BundledPrograms.hs
+++ Build/BundledPrograms.hs
@@ -17,7 +17,26 @@ import Build.SysConfig as SysConfig
  -
  - These may be just the command name, or the full path to it. -}
 bundledPrograms :: [FilePath]
-bundledPrograms = catMaybes
+bundledPrograms = preferredBundledPrograms ++ extraBundledPrograms
+
+{- Programs that are only included in the bundle in case the system
+ - doesn't have them. These come after the system PATH.
+ -}
+extraBundledPrograms :: [FilePath]
+extraBundledPrograms = catMaybes
+	-- The system gpg is probably better, because it may better
+	-- integrate with the system gpg-agent, etc.
+	[ SysConfig.gpg
+	]
+
+{- Programs that should be preferred for use from the bundle, over
+ - any that might be installed on the system otherwise. These come before
+ - the system PATH.
+ -
+ - For example, git-annex is built for a specific version of git.
+ -}
+preferredBundledPrograms :: [FilePath]
+preferredBundledPrograms = catMaybes
 	[ Nothing
 #ifndef mingw32_HOST_OS
 	-- git is not included in the windows bundle; git for windows is used
@@ -56,7 +75,6 @@ bundledPrograms = catMaybes
 #ifndef mingw32_HOST_OS
 	-- All these utilities are included in git for Windows
 	, ifset SysConfig.curl "curl"
-	, SysConfig.gpg
 	, SysConfig.sha1
 	, SysConfig.sha256
 	, SysConfig.sha512
