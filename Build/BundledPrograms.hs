ae0a10618b2da00cd9ad23b7801eb444cb5000cc
Joey Hess <joeyh@joeyh.name>
2016-02-23 12:48:18 -0400

    fix build more

--- Build/BundledPrograms.hs
+++ Build/BundledPrograms.hs
@@ -29,8 +29,10 @@ extraBundledPrograms = catMaybes
 	-- On Windows, gpg is bundled with git for windows.
 #ifndef mingw32_HOST_OS
 	[ SysConfig.gpg
-	]
+#else
+	[
 #endif
+	]
 
 {- Programs that should be preferred for use from the bundle, over
  - any that might be installed on the system otherwise. These come before
