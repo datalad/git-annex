78fa865721d49c5382e495c71f58a8e80b6db071
Joey Hess <joeyh@joeyh.name>
2016-03-06 12:45:57 -0400

    dropkey: Add --batch.

--- Command/DropKey.hs
+++ Command/DropKey.hs
@@ -1,6 +1,6 @@
 {- git-annex command
  -
- - Copyright 2010 Joey Hess <id@joeyh.name>
+ - Copyright 2010,2016 Joey Hess <id@joeyh.name>
  -
  - Licensed under the GNU GPL version 3 or higher.
  -}
@@ -13,26 +13,45 @@ import Logs.Location
 import Annex.Content
 
 cmd :: Command
-cmd = noCommit $ 
+cmd = noCommit $
 	command "dropkey" SectionPlumbing
 		"drops annexed content for specified keys"
 		(paramRepeating paramKey)
-		(withParams seek)
+		(seek <$$> optParser)
 
-seek :: CmdParams -> CommandSeek
-seek = withKeys start
+data DropKeyOptions = DropKeyOptions
+	{ toDrop :: [String]
+	, batchOption :: BatchMode
+	}
 
-start :: Key -> CommandStart
-start key = stopUnless (inAnnex key) $ do
+optParser :: CmdParamsDesc -> Parser DropKeyOptions
+optParser desc = DropKeyOptions
+	<$> cmdParams desc
+	<*> parseBatchOption
+
+seek :: DropKeyOptions -> CommandSeek
+seek o = do
 	unlessM (Annex.getState Annex.force) $
 		error "dropkey can cause data loss; use --force if you're sure you want to do this"
+	withKeys start (toDrop o)
+	case batchOption o of
+		Batch -> batchInput parsekey $ batchCommandAction . start
+		NoBatch -> noop
+  where
+	parsekey = maybe (Left "bad key") Right . file2key
+
+start :: Key -> CommandStart
+start key = do
 	showStart' "dropkey" key Nothing
 	next $ perform key
 
 perform :: Key -> CommandPerform
-perform key = lockContentForRemoval key $ \contentlock -> do
-	removeAnnex contentlock
-	next $ cleanup key
+perform key = ifM (inAnnex key)
+	( lockContentForRemoval key $ \contentlock -> do
+		removeAnnex contentlock
+		next $ cleanup key
+	, next $ return True
+	)
 
 cleanup :: Key -> CommandCleanup
 cleanup key = do
