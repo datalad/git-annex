6623b557eda337ded02819749c6f03fc138a30b8
Joey Hess <joeyh@joeyh.name>
2016-03-08 02:45:10 -0400

    build without disk-free-space on android

--- Utility/DiskFree.hs
+++ Utility/DiskFree.hs
@@ -6,11 +6,14 @@
  -}
 
 {-# OPTIONS_GHC -fno-warn-tabs #-}
+{-# LANGUAGE CPP #-}
 
 module Utility.DiskFree (
 	getDiskFree,
 	getDiskSize
 ) where
+
+#ifndef __ANDROID__
 
 import System.DiskSpace
 import Utility.Applicative
@@ -21,3 +24,15 @@ getDiskFree = catchMaybeIO . getAvailSpace
 
 getDiskSize :: FilePath -> IO (Maybe Integer)
 getDiskSize = fmap diskTotal <$$> catchMaybeIO . getDiskUsage
+
+#else
+
+#warning Building without disk free space checking support
+
+getDiskFree :: FilePath -> IO (Maybe Integer)
+getDiskFree _ = return Nothing
+
+getDiskSize :: FilePath -> IO (Maybe Integer)
+getDiskSize _ = return Nothing
+
+#endif
