0e04b22491bd618242f52931c8786a476d7fa970
Joey Hess <joeyh@joeyh.name>
2016-12-24 13:08:07 -0400

    fix su params

--- Utility/Su.hs
+++ Utility/Su.hs
@@ -45,7 +45,7 @@ runAsRoot cmd ps = go =<< firstM (inPath . fst) =<< selectcmds
 	
 	-- These will only work when run in a console.
 	consolecmds = 
-		[ ("su", [Param "-c", Param "--", Param cmd] ++ ps)
+		[ ("su", [Param "-c", Param cmd, Param "--"] ++ ps)
 		, ("sudo", [Param cmd] ++ ps)
 		, ("su-to-root", [Param "-c", Param shellcmd])
 		]
