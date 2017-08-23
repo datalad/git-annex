24a66349349443698060d8384e22e53606297c43
Joey Hess <joeyh@joeyh.name>
2017-08-23 11:31:14 -0400

    stop removing cabal

--- standalone/windows/build.sh
+++ standalone/windows/build.sh
@@ -27,8 +27,8 @@ export UPGRADE_LOCATION
 #export FORCE_GIT_VERSION
 
 # Uncomment to get rid of cabal installed libraries.
-cabal list --installed
-rm -rf /c/Users/jenkins/AppData/Roaming/cabal /c/Users/jenkins/AppData/Roaming/ghc
+#cabal list --installed
+#rm -rf /c/Users/jenkins/AppData/Roaming/cabal /c/Users/jenkins/AppData/Roaming/ghc
 
 # Don't allow build artifact from a past successful build to be extracted
 # if we fail.
