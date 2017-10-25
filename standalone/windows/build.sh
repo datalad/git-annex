3761f5a05208155582715450ad9c771345429f1c
Joey Hess <joeyh@joeyh.name>
2017-10-25 00:09:58 -0400

    try different path

--- standalone/windows/build.sh
+++ standalone/windows/build.sh
@@ -6,7 +6,7 @@
 set -x
 set -e
 
-PATH="/Program Files (x86)/NSIS/Bin:/Program Files (x86)/NSIS:/usr/local/bin:/usr/bin:$PATH"
+PATH="/cygdrive/c/Program Files (x86)/NSIS/Bin:/cygdrive/c/Program Files (x86)/NSIS:/usr/local/bin:/usr/bin:$PATH"
 
 # Run a command with the cygwin environment available.
 # However, programs not from cygwin are preferred.
