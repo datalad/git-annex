061f5ddd4374a880df8f3ce0abba2f68872fbb78
Joey Hess <joeyh@joeyh.name>
2017-11-14 14:53:36 -0400

    avoid Utility.Directory which needs Win32 on windows
    custom-setup can't depend on Win32 due to cabal limitations

--- Build/Configure.hs
+++ Build/Configure.hs
@@ -7,6 +7,7 @@ module Build.Configure where
 import Control.Applicative
 import Control.Monad.IfElse
 import Control.Monad
+import System.Directory
 
 import Build.TestConfig
 import Build.Version
@@ -18,7 +19,6 @@ import Utility.Env
 import Utility.Exception
 import qualified Git.Version
 import Utility.DottedVersion
-import Utility.Directory
 
 tests :: [TestCase]
 tests =
