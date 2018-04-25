096bb0aa21d3e69ed91fc298ace1a749fcf40131
Joey Hess <joeyh@joeyh.name>
2018-04-25 16:57:57 -0400

    improve comment

--- Utility/Android.hs
+++ Utility/Android.hs
@@ -10,5 +10,8 @@ module Utility.Android where
 import Common
 
 -- Detect when the Linux build is running on Android, eg in termux.
+--
+-- Note that this relies on termux's uname having been built with "Android"
+-- as the os name. Often on Android, uname will report "Linux".
 osAndroid :: IO Bool
 osAndroid = ("Android" `isPrefixOf` ) <$> readProcess "uname" ["-o"]
