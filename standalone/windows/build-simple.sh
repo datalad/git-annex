663b0353c9ffad928abfe32a734eb5b656e4620e
Joey Hess <joeyh@joeyh.name>
2019-07-22 09:28:00 -0400

    remove cygwin stuff from this script
    cygwin in not needed to build git-annex on windows AFAIK

--- standalone/windows/build-simple.sh
+++ standalone/windows/build-simple.sh
@@ -6,25 +6,11 @@ set -x
 
 PATH="/c/Program Files/Git/cmd:/c/Program Files/NSIS:$PATH"
 
-# Run a command with the cygwin environment available.
-# However, programs not from cygwin are preferred.
-withcyg () {
-	PATH="$PATH:/c/cygwin/bin" "$@"
-}
-
-# Prefer programs from cygwin.
-withcygpreferred () {
-	PATH="/c/cygwin/bin:$PATH" "$@"
-}
-
-# Deps are not built with cygwin environment, because we don't want
-# configure scripts for haskell libraries to link them with the cygwin
-# libraries.
 stack setup
 stack build --dependencies-only
 
 # Build git-annex
-withcyg stack build
+stack build
 
 # Build the installer
-withcygpreferred stack runghc --package nsis Build/NullSoftInstaller.hs
+stack runghc --package nsis Build/NullSoftInstaller.hs
