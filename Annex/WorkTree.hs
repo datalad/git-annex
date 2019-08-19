datalad/git-annex

commit 7be690f326da3a93b44dabc824d433246a8f502a

check headRef not Branch.current
Support running v7 upgrade in a repo where there is no branch checked out,
but HEAD is set directly to some other ref.

This commit was sponsored by Jack Hill on Patreon.

Annex/WorkTree.hs
@@ -73,13 +73,13 @@ ifAnnexed file yes no = maybe no yes =<< lookupFile file
  - 
  - This is expensive, and so normally the associated files are updated
  - incrementally when changes are noticed. So, this only needs to be done
- - when initializing/upgrading a v6 mode repository.
+ - when initializing/upgrading a v6+ mode repository.
  -
  - Also, the content for the unlocked file may already be present as
  - an annex object. If so, make the unlocked file use that content.
  -}
 scanUnlockedFiles :: Annex ()
-scanUnlockedFiles = whenM (isJust <$> inRepo Git.Branch.current) $ do
+scanUnlockedFiles = whenM (inRepo $ Git.Ref.exists Git.Ref.headRef) $ do
 	showSideAction "scanning for unlocked files"
 	Database.Keys.runWriter $
 		liftIO . Database.Keys.SQL.dropAllAssociatedFiles

doc/bugs/get_fails_to_place_v7_unlocked_file_content_into_the_file_tree_in_v7_in_repo_with_detached_HEAD.mdwn
@@ -54,3 +54,5 @@ as you can see the arg-test.sim is only 68 bytes, not 2MB or a symlink.
 
 
 [[!meta author=yoh]]
+
+> [[fixed|done]] --[[Joey]]

doc/bugs/get_fails_to_place_v7_unlocked_file_content_into_the_file_tree_in_v7_in_repo_with_detached_HEAD/comment_3_cd7cc008c3d37caa76416bd2452da90d._comment
@@ -1,0 +1,21 @@
+[[!comment format=mdwn
+ username="joey"
+ subject="""comment 3"""
+ date="2019-07-16T16:13:49Z"
+ content="""
+I notice that the keys database is not populated in the clone.
+
+Also, the upgrade does not display "scanning for unlocked files".
+And in Annex.WorkTree, we can see why:
+
+	scanUnlockedFiles = whenM (isJust <$> inRepo Git.Branch.current) $ do
+	        showSideAction "scanning for unlocked files"
+
+There is no current git branch in this case.
+
+That check was added in [[!commit 9b995954731e05727d77c7bff487af10da9cb4b9]]
+"only do scan when there's a branch, not in freshly created new repo"
+
+Since it does a ls-tree of HEAD, what it really ought to check for is that
+HEAD is set, which it's not in a fresh new repo. Done.
+"""]]

commit bb16a261099e77f6d79c959547f9a9f36e00c7d0

use headExists
Turns out that 7be690f326da3a93b44dabc824d433246a8f502a broke the
test suite on the i386ancient builder. There, git show-ref --verify HEAD
fails with "'HEAD' - not a valid ref". Apparently git 2.1.4 didn't
support that.

headExists works there and does the same thing.

Annex/WorkTree.hs
@@ -78,7 +78,7 @@ ifAnnexed file yes no = maybe no yes =<< lookupFile file
  - an annex object. If so, make the unlocked file use that content.
  -}
 scanUnlockedFiles :: Annex ()
-scanUnlockedFiles = whenM (inRepo $ Git.Ref.exists Git.Ref.headRef) $ do
+scanUnlockedFiles = whenM (inRepo Git.Ref.headExists) $ do
 	showSideAction "scanning for unlocked files"
 	Database.Keys.runWriter $
 		liftIO . Database.Keys.SQL.dropAllAssociatedFiles