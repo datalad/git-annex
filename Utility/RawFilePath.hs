360942ba127c4a5f4cbf820b9706de4f4eb96c77
Joey Hess <joeyh@joeyh.name>
2019-12-06 14:17:48 -0400

    RawFilePath will need to support Windows too
    Of course, readSymbolicLink always fails on Windows, but now it's ready
    for other things that don't fail there.

--- Utility/RawFilePath.hs
+++ Utility/RawFilePath.hs
@@ -1,5 +1,12 @@
 {- Portability shim around System.Posix.Files.ByteString
  -
+ - On unix, this makes syscalls using RawFilesPaths as efficiently as
+ - possible.
+ -
+ - On Windows, filenames are in unicode, so RawFilePaths have to be
+ - decoded. So this library will work, but less efficiently than using
+ - FilePath would.
+ -
  - Copyright 2019 Joey Hess <id@joeyh.name>
  -
  - License: BSD-2-clause
@@ -17,12 +24,9 @@ import System.Posix.Files.ByteString
 import System.Posix.ByteString.FilePath
 #else
 import qualified Data.ByteString as B
-import System.IO.Error
-
-type RawFilePath = B.ByteString
+import qualified System.PosixCompat as P
+import Utility.FileSystemEncoding
 
 readSymbolicLink :: RawFilePath -> IO RawFilePath
-readSymbolicLink _ = ioError $ mkIOError illegalOperationErrorType x Nothing Nothing
-  where
-	x = "Utility.RawFilePath.readSymbolicLink: not supported"
+readSymbolicLink f = toRawFilePath <$> P.readSymbolicLink (fromRawFilePath f)
 #endif
