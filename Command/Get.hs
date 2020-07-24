2d771a7d3227628c07ef978311d579e50220c285
Joey Hess <joeyh@joeyh.name>
2020-07-24 11:49:15 -0400

    add back inAnnex check for keys options
    Lost in recent commit.

--- Command/Get.hs
+++ Command/Get.hs
@@ -66,7 +66,8 @@ start o from file key = start' expensivecheck from key afile ai
 
 startKeys :: Maybe Remote -> (Key, ActionItem) -> CommandStart
 startKeys from (key, ai) = checkFailedTransferDirection ai Download $
-	start' (return True) from key (AssociatedFile Nothing) ai
+	stopUnless (not <$> inAnnex key) $
+		start' (return True) from key (AssociatedFile Nothing) ai
 
 start' :: Annex Bool -> Maybe Remote -> Key -> AssociatedFile -> ActionItem -> CommandStart
 start' expensivecheck from key afile ai =
