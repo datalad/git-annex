922621301ae750e9ee86807ae21709e2e5b2d352
Joey Hess <joeyh@joeyh.name>
2020-09-17 17:27:42 -0400

    Serialize use of C magic library, which is not thread safe.
    This fixes failures uploading to S3 when using -J.
    
    This commit was sponsored by Denis Dzyubenko on Patreon.

--- Annex/Magic.hs
+++ Annex/Magic.hs
@@ -1,6 +1,6 @@
 {- Interface to libmagic
  -
- - Copyright 2019 Joey Hess <id@joeyh.name>
+ - Copyright 2019-2020 Joey Hess <id@joeyh.name>
  -
  - Licensed under the GNU AGPL version 3 or higher.
  -}
@@ -21,6 +21,8 @@ import Control.Monad.IO.Class
 #ifdef WITH_MAGICMIME
 import Magic
 import Utility.Env
+import Control.Concurrent
+import System.IO.Unsafe (unsafePerformIO)
 import Common
 #else
 type Magic = ()
@@ -41,7 +43,7 @@ initMagicMime = return Nothing
 
 getMagicMime :: Magic -> FilePath -> IO (Maybe (MimeType, MimeEncoding))
 #ifdef WITH_MAGICMIME
-getMagicMime m f = Just . parse <$> magicFile m f
+getMagicMime m f = Just . parse <$> magicConcurrentSafe (magicFile m f)
   where
 	parse s = 
 		let (mimetype, rest) = separate (== ';') s
@@ -58,3 +60,15 @@ getMagicMimeType m f = liftIO $ fmap fst <$> getMagicMime m f
 
 getMagicMimeEncoding :: MonadIO m => Magic -> FilePath -> m(Maybe MimeEncoding)
 getMagicMimeEncoding m f = liftIO $ fmap snd <$> getMagicMime m f
+
+#ifdef WITH_MAGICMIME
+{-# NOINLINE mutex #-}
+mutex :: MVar ()
+mutex = unsafePerformIO $ newMVar ()
+
+-- Work around a bug, the library is not concurrency safe and will
+-- sometimes access the wrong memory if multiple ones are called at the
+-- same time.
+magicConcurrentSafe :: IO a -> IO a
+magicConcurrentSafe = bracket_ (takeMVar mutex) (putMVar mutex ())
+#endif
