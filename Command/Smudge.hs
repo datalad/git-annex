datalad/git-annex

commit 34a535ebeaf483f6643f949ae774939650d74837

adjust: Fix some bad behavior when unlocked files use URL keys.
This avoids the smudge --clean filter failing on the URL keys.

git checkout runs the post-checkout hook, which runs smudge --update.
That populates all the pointer files, but it neglected to store their inode
caches in the keys db. With that done, and the keys db flushed before
smudge --clean gets run (by restagePointerFile), the isUnmodifiedCheap
check can tell the file is not modified, so will not try to re-ingest it,
which does not work with URL keys because they do not support genKey.

It also seems possible that the isUnmodifiedCheap was also failing for
non-URL keys, which would cause them to be re-ingested, leading to a lot of
extra work. I have not verified that, but don't see why it wouldn't have
happened. So this probably also speeds up checking out adjusted branches.

This commit was sponsored by Boyd Stephen Smith Jr. on Patreon.

Command/Smudge.hs
@@ -255,8 +255,9 @@ updateSmudged restage = streamSmudged $ \k topf -> do
 	f <- fromRepo (fromTopFilePath topf)
 	whenM (inAnnex k) $ do
 		obj <- calcRepo (gitAnnexLocation k)
-		unlessM (isJust <$> populatePointerFile restage k obj f) $
-			liftIO (isPointerFile f) >>= \case
+		populatePointerFile restage k obj f >>= \case
+			Just ic -> Database.Keys.addInodeCaches k [ic]
+			Nothing -> liftIO (isPointerFile f) >>= \case
 				Just k' | k' == k -> toplevelWarning False $
 					"unable to populate worktree file " ++ fromRawFilePath f
 				_ -> noop

Annex/Link.hs
@@ -35,6 +35,7 @@ import Utility.FileMode
 import Utility.InodeCache
 import Utility.Tmp.Dir
 import Utility.CopyFile
+import qualified Database.Keys.Handle
 import qualified Utility.RawFilePath as R
 
 import qualified Data.ByteString as S
@@ -202,6 +203,8 @@ restagePointerFile (Restage True) f orig = withTSDelta $ \tsd ->
 	-- updated index file.
 	runner :: Git.Queue.InternalActionRunner Annex
 	runner = Git.Queue.InternalActionRunner "restagePointerFile" $ \r l -> do
+		liftIO . Database.Keys.Handle.flushDbQueue
+			=<< Annex.getState Annex.keysdbhandle
 		realindex <- liftIO $ Git.Index.currentIndexFile r
 		let lock = fromRawFilePath (Git.Index.indexFileLock realindex)
 		    lockindex = liftIO $ catchMaybeIO $ Git.LockFile.openLock' lock

commit d4aac6428221f9069da85181ed37163bd8f11d58

fix breakage caused by recent commit
34a535ebeaf483f6643f949ae774939650d74837 broke the test suite.
Getting a file started failing in one case, because the annex object did
not have its inode cached, so was not trusted to be unmodified.

This adds something very similar to what was added to linkAnnex
in commit 2e9341a47d437b5a7b14824d3e5b2864b059a850 -- if there are not
yet any inodes cached for a key, add the inode of the annex object when
adding the inode of the unlocked file.

Feels like this should be handled in a more principled way. How
do we know the addInodeCaches call in getMoveRaceRecovery just above
this change is currently correct? It doesn't add the annex object inode
cache. Ah well, maybe sometime when I've not had my entire evening eaten
by a reversion that the test suite caught as I was cooking dinner.

Command/Smudge.hs
@@ -259,8 +259,13 @@ updateSmudged restage = streamSmudged $ \k topf -> do
 	f <- fromRepo (fromTopFilePath topf)
 	whenM (inAnnex k) $ do
 		obj <- calcRepo (gitAnnexLocation k)
+		objic <- withTSDelta (liftIO . genInodeCache obj)
 		populatePointerFile restage k obj f >>= \case
-			Just ic -> Database.Keys.addInodeCaches k [ic]
+			Just ic -> do
+				cs <- Database.Keys.getInodeCaches k
+				if null cs
+					then Database.Keys.addInodeCaches k (catMaybes [Just ic, objic])
+					else Database.Keys.addInodeCaches k [ic]
 			Nothing -> liftIO (isPointerFile f) >>= \case
 				Just k' | k' == k -> toplevelWarning False $
 					"unable to populate worktree file " ++ fromRawFilePath f