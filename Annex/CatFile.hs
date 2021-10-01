datalad/git-annex

commit 4bf7940d6b912fbf692b268f621ebd41ed871125

fileRef: make paths relative and simplified
Fix behavior of several commands, including reinject, addurl, and rmurl
when given an absolute path to an unlocked file, or a relative path that
leaves and re-enters the repository.

To avoid slowing down all the cases where the paths are already ok
with an unncessary call to getCurrentDirectory, put in an optimisation
in relPathCwdToFile. That will probably also speed up other parts of
git-annex by some small amount, but I have not benchmarked.

Note that I did not convert branchFileRef, because it seems likely that
it will be used with a file that is not provided by the user, so is already
in a sane format. This is certainly true for the way git-annex uses it,
though maybe arguable to the extent Git.Ref is a reusable library.

Annex/CatFile.hs
@@ -178,11 +178,11 @@ catSymLinkTarget sha = fromInternalGitPath . L.toStrict <$> get
 catKeyFile :: RawFilePath -> Annex (Maybe Key)
 catKeyFile f = ifM (Annex.getState Annex.daemon)
 	( catKeyFileHEAD f
-	, catKey $ Git.Ref.fileRef f
+	, catKey =<< liftIO (Git.Ref.fileRef f)
 	)
 
 catKeyFileHEAD :: RawFilePath -> Annex (Maybe Key)
-catKeyFileHEAD f = catKey $ Git.Ref.fileFromRef Git.Ref.headRef f
+catKeyFileHEAD f = catKey =<< liftIO (Git.Ref.fileFromRef Git.Ref.headRef f)
 
 {- Look in the original branch from whence an adjusted branch is based
  - to find the file. But only when the adjustment hides some files. -}
@@ -194,5 +194,6 @@ catObjectMetaDataHidden = hiddenCat catObjectMetaData
 
 hiddenCat :: (Ref -> Annex (Maybe a)) -> RawFilePath -> CurrBranch -> Annex (Maybe a)
 hiddenCat a f (Just origbranch, Just adj)
-	| adjustmentHidesFiles adj = a (Git.Ref.fileFromRef origbranch f)
+	| adjustmentHidesFiles adj = 
+		a =<< liftIO (Git.Ref.fileFromRef origbranch f)
 hiddenCat _ _ _ = return Nothing

Command/Smudge.hs
@@ -103,7 +103,7 @@ clean file = do
 				getMoveRaceRecovery k file
 				liftIO $ L.hPut stdout b
 		Nothing -> do
-			let fileref = Git.Ref.fileRef file
+			fileref <- liftIO $ Git.Ref.fileRef file
 			indexmeta <- catObjectMetaData fileref
 			oldkey <- case indexmeta of
 				Just (_, sz, _) -> catKey' fileref sz

Git/Ref.hs
@@ -64,12 +64,17 @@ branchRef = underBase "refs/heads"
 
 {- A Ref that can be used to refer to a file in the repository, as staged
  - in the index.
- -
- - Prefixing the file with ./ makes this work even if in a subdirectory
- - of a repo.
  -}
-fileRef :: RawFilePath -> Ref
-fileRef f = Ref $ ":./" <> toInternalGitPath f
+fileRef :: RawFilePath -> IO Ref
+fileRef f = do
+	-- The filename could be absolute, or contain eg "../repo/file",
+	-- neither of which work in a ref, so convert it to a minimal
+	-- relative path.
+	f' <- relPathCwdToFile f
+ 	-- Prefixing the file with ./ makes this work even when in a
+	-- subdirectory of a repo. Eg, ./foo in directory bar refers
+	-- to bar/foo, not to foo in the top of the repository.
+	return $ Ref $ ":./" <> toInternalGitPath f'
 
 {- A Ref that can be used to refer to a file in a particular branch. -}
 branchFileRef :: Branch -> RawFilePath -> Ref
@@ -81,8 +86,10 @@ dateRef r (RefDate d) = Ref $ fromRef' r <> "@" <> encodeBS' d
 
 {- A Ref that can be used to refer to a file in the repository as it
  - appears in a given Ref. -}
-fileFromRef :: Ref -> RawFilePath -> Ref
-fileFromRef r f = let (Ref fr) = fileRef f in Ref (fromRef' r <> fr)
+fileFromRef :: Ref -> RawFilePath -> IO Ref
+fileFromRef r f = do
+	(Ref fr) <- fileRef f
+	return (Ref (fromRef' r <> fr))
 
 {- Checks if a ref exists. Note that it must be fully qualified,
  - eg refs/heads/master rather than master. -}

Utility/Path/AbsRel.hs
@@ -1,6 +1,6 @@
 {- absolute and relative path manipulation
  -
- - Copyright 2010-2020 Joey Hess <id@joeyh.name>
+ - Copyright 2010-2021 Joey Hess <id@joeyh.name>
  -
  - License: BSD-2-clause
  -}
@@ -19,6 +19,7 @@ module Utility.Path.AbsRel (
 ) where
 
 import System.FilePath.ByteString
+import qualified Data.ByteString as B
 #ifdef mingw32_HOST_OS
 import System.Directory (getCurrentDirectory)
 #else
@@ -72,13 +73,17 @@ absPath file
  -    relPathCwdToFile "../bar/baz" == "baz"
  -}
 relPathCwdToFile :: RawFilePath -> IO RawFilePath
-relPathCwdToFile f = do
+relPathCwdToFile f
+	-- Optimisation: Avoid doing any IO when the path is relative
+	-- and does not contain any ".." component.
+	| isRelative f && not (".." `B.isInfixOf` f) = return f
+	| otherwise = do
 #ifdef mingw32_HOST_OS
-	c <- toRawFilePath <$> getCurrentDirectory
+		c <- toRawFilePath <$> getCurrentDirectory
 #else
-	c <- getWorkingDirectory
+		c <- getWorkingDirectory
 #endif
-	relPathDirToFile c f
+		relPathDirToFile c f
 
 {- Constructs a minimal relative path from a directory to a file. -}
 relPathDirToFile :: RawFilePath -> RawFilePath -> IO RawFilePath

commit 9012fa01877a78542c64f104bd60a80825ce3c86

reinject: Fix crash when reinjecting a file from outside the repository
Commit 4bf7940d6b912fbf692b268f621ebd41ed871125 introduced this
problem, but was otherwise doing a good thing. Problem being
that fileRef "/foo" used to return ":./foo", which was actually wrong,
but as long as there was no foo in the local repository, catKey
could operate on it without crashing. After that fix though, fileRef
would return eg "../../foo", resulting in fileRef returning
":./../../foo", which will make git cat-file crash since that's
not a valid path in the repo.

Fix is simply to make fileRef detect paths outside the repo and return
Nothing. Then catKey can be skipped. This needed several bugfixes to
dirContains as well, in previous commits.

In Command.Smudge, this led to needing to check for Nothing. That case
should actually never happen, because the fileoutsiderepo check will
detect it earlier.

Sponsored-by: Brock Spratlen on Patreon

Annex/CatFile.hs
@@ -198,11 +198,12 @@ catSymLinkTarget sha = fromInternalGitPath . L.toStrict <$> get
 catKeyFile :: RawFilePath -> Annex (Maybe Key)
 catKeyFile f = ifM (Annex.getState Annex.daemon)
 	( catKeyFileHEAD f
-	, catKey =<< liftIO (Git.Ref.fileRef f)
+	, maybe (pure Nothing) catKey =<< inRepo (Git.Ref.fileRef f)
 	)
 
 catKeyFileHEAD :: RawFilePath -> Annex (Maybe Key)
-catKeyFileHEAD f = catKey =<< liftIO (Git.Ref.fileFromRef Git.Ref.headRef f)
+catKeyFileHEAD f = maybe (pure Nothing) catKey
+	=<< inRepo (Git.Ref.fileFromRef Git.Ref.headRef f)
 
 {- Look in the original branch from whence an adjusted branch is based
  - to find the file. But only when the adjustment hides some files. -}
@@ -215,5 +216,6 @@ catObjectMetaDataHidden = hiddenCat catObjectMetaData
 hiddenCat :: (Ref -> Annex (Maybe a)) -> RawFilePath -> CurrBranch -> Annex (Maybe a)
 hiddenCat a f (Just origbranch, Just adj)
 	| adjustmentHidesFiles adj = 
-		a =<< liftIO (Git.Ref.fileFromRef origbranch f)
+		maybe (pure Nothing) a
+			=<< inRepo (Git.Ref.fileFromRef origbranch f)
 hiddenCat _ _ _ = return Nothing

Command/Smudge.hs
@@ -105,13 +105,14 @@ clean file = do
 			addingExistingLink file k $ do
 				getMoveRaceRecovery k file
 				liftIO $ L.hPut stdout b
-		Nothing -> do
-			fileref <- liftIO $ Git.Ref.fileRef file
-			indexmeta <- catObjectMetaData fileref
-			oldkey <- case indexmeta of
-				Just (_, sz, _) -> catKey' fileref sz
-				Nothing -> return Nothing
-			go' b indexmeta oldkey
+		Nothing -> inRepo (Git.Ref.fileRef file) >>= \case
+			Just fileref -> do
+				indexmeta <- catObjectMetaData fileref
+				oldkey <- case indexmeta of
+					Just (_, sz, _) -> catKey' fileref sz
+					Nothing -> return Nothing
+				go' b indexmeta oldkey
+			Nothing -> liftIO $ L.hPut stdout b
 	go' b indexmeta oldkey = ifM (shouldAnnex file indexmeta oldkey)
 		( do
 			-- Before git 2.5, failing to consume all stdin here

Git.hs
@@ -71,7 +71,7 @@ repoLocation Repo { location = LocalUnknown dir } = fromRawFilePath dir
 repoLocation Repo { location = Unknown } = error "unknown repoLocation"
 
 {- Path to a repository. For non-bare, this is the worktree, for bare, 
- - it's the gitdit, and for URL repositories, is the path on the remote
+ - it's the gitdir, and for URL repositories, is the path on the remote
  - host. -}
 repoPath :: Repo -> RawFilePath
 repoPath Repo { location = Url u } = toRawFilePath $ unEscapeString $ uriPath u

Git/Ref.hs
@@ -64,17 +64,22 @@ branchRef = underBase "refs/heads"
 
 {- A Ref that can be used to refer to a file in the repository, as staged
  - in the index.
+ - 
+ - If the input file is located outside the repository, returns Nothing.
  -}
-fileRef :: RawFilePath -> IO Ref
-fileRef f = do
+fileRef :: RawFilePath -> Repo -> IO (Maybe Ref)
+fileRef f repo = do
 	-- The filename could be absolute, or contain eg "../repo/file",
 	-- neither of which work in a ref, so convert it to a minimal
 	-- relative path.
 	f' <- relPathCwdToFile f
- 	-- Prefixing the file with ./ makes this work even when in a
-	-- subdirectory of a repo. Eg, ./foo in directory bar refers
-	-- to bar/foo, not to foo in the top of the repository.
-	return $ Ref $ ":./" <> toInternalGitPath f'
+	print ("f'", f', repoPath repo, repoPath repo `dirContains` f')
+	return $ if repoPath repo `dirContains` f'
+ 		-- Prefixing the file with ./ makes this work even when in a
+		-- subdirectory of a repo. Eg, ./foo in directory bar refers
+		-- to bar/foo, not to foo in the top of the repository.
+		then Just $ Ref $ ":./" <> toInternalGitPath f'
+		else Nothing
 
 {- A Ref that can be used to refer to a file in a particular branch. -}
 branchFileRef :: Branch -> RawFilePath -> Ref
@@ -85,11 +90,14 @@ dateRef :: Ref -> RefDate -> Ref
 dateRef r (RefDate d) = Ref $ fromRef' r <> "@" <> encodeBS d
 
 {- A Ref that can be used to refer to a file in the repository as it
- - appears in a given Ref. -}
-fileFromRef :: Ref -> RawFilePath -> IO Ref
-fileFromRef r f = do
-	(Ref fr) <- fileRef f
-	return (Ref (fromRef' r <> fr))
+ - appears in a given Ref. 
+ -
+ - If the file path is located outside the repository, returns Nothing.
+ -}
+fileFromRef :: Ref -> RawFilePath -> Repo -> IO (Maybe Ref)
+fileFromRef r f repo = fileRef f repo >>= return . \case
+	Just (Ref fr) -> Just (Ref (fromRef' r <> fr))
+	Nothing -> Nothing
 
 {- Checks if a ref exists. Note that it must be fully qualified,
  - eg refs/heads/master rather than master. -}

doc/todo/recent_change_to_fileRef_breaks_reinject_--known.mdwn
@@ -24,3 +24,5 @@ By means of bisection I have determined that commit 4bf7940d6b912fbf692b268f621e
     git-annex: fd:15: Data.ByteString.hGetLine: end of file
 
 --spwhitton
+
+> [[fixed|done]] --[[Joey]]

doc/todo/recent_change_to_fileRef_breaks_reinject_--known/comment_1_5d3ade0367d3913bc115082ef2abd842._comment
@@ -1,0 +1,11 @@
+[[!comment format=mdwn
+ username="joey"
+ subject="""comment 1"""
+ date="2021-10-01T16:42:36Z"
+ content="""
+Also happens with a relative path to the file. And also 
+`git annex reinject ../bar bar` fails the same way.
+
+Fixed. In case you want to cherry-pick the fix, it's the commit adding
+this comment, as well as the 2 prior commits fixing bugs in dirContains.
+"""]]