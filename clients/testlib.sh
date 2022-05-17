085dc60c894e0630f2d4ade1c2c2ef999b1007e1
Yaroslav Halchenko <debian@onerussian.com>
2022-05-17 11:49:43 -0400

    Better wording
    Co-authored-by: John T. Wodder II <jwodder@users.noreply.github.com>

--- clients/testlib.sh
+++ clients/testlib.sh
@@ -2,6 +2,6 @@ function workdir_base {
     WORKDIR_BASE="$1"
     mkdir -p "$WORKDIR_BASE/$BUILDNO"
     SECONDS=0
-    trap 'echo "Elapsed $SECONDS seconds" && cd "$WORKDIR_BASE" && chmod -R +w "$BUILDNO" && rm -rf "$BUILDNO"' EXIT
+    trap 'echo "Elapsed time: $SECONDS seconds" && cd "$WORKDIR_BASE" && chmod -R +w "$BUILDNO" && rm -rf "$BUILDNO"' EXIT
     cd "$WORKDIR_BASE/$BUILDNO"
 }
