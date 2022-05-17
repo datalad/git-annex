1dbe1687ec4c66168178db5410922aa6ba7a8c29
Yaroslav Halchenko <debian@onerussian.com>
2022-05-17 08:29:51 -0400

    ENH: clients/testlib.sh inform on how long it took before trap exit

--- clients/testlib.sh
+++ clients/testlib.sh
@@ -1,6 +1,7 @@
 function workdir_base {
     WORKDIR_BASE="$1"
     mkdir -p "$WORKDIR_BASE/$BUILDNO"
-    trap 'cd "$WORKDIR_BASE" && chmod -R +w "$BUILDNO" && rm -rf "$BUILDNO"' EXIT
+    SECONDS=0
+    trap 'echo "Elapsed $SECONDS seconds" && cd "$WORKDIR_BASE" && chmod -R +w "$BUILDNO" && rm -rf "$BUILDNO"' EXIT
     cd "$WORKDIR_BASE/$BUILDNO"
 }
