datalad/git-annex

commit ba7ecbc6a9c3763e8152e4f46522d14a4ee2b59d

avoid flushing keys db queue after each Annex action
The flush was only done Annex.run' to make sure that the queue was flushed
before git-annex exits. But, doing it there means that as soon as one
change gets queued, it gets flushed soon after, which contributes to
excessive writes to the database, slowing git-annex down.
(This does not yet speed git-annex up, but it is a stepping stone to
doing so.)

Database queues do not autoflush when garbage collected, so have to
be flushed explicitly. I don't think it's possible to make them
autoflush (except perhaps if git-annex sqitched to using ResourceT..).
The comment in Database.Keys.closeDb used to be accurate, since the
automatic flushing did mean that all writes reached the database even
when closeDb was not called. But now, closeDb or flushDb needs to be
called before stopping using an Annex state. So, removed that comment.

In Remote.Git, change to using quiesce everywhere that it used to use
stopCoProcesses. This means that uses on onLocal in there are just as
slow as before. I considered only calling closeDb on the local git remotes
when git-annex exits. But, the reason that Remote.Git calls stopCoProcesses
in each onLocal is so as not to leave git processes running that have files
open on the remote repo, when it's on removable media. So, it seemed to make
sense to also closeDb after each one, since sqlite may also keep files
open. Although that has not seemed to cause problems with removable
media so far. It was also just easier to quiesce in each onLocal than
once at the end. This does likely leave performance on the floor, so
could be revisited.

In Annex.Content.saveState, there was no reason to close the db,
flushing it is enough.

The rest of the changes are from auditing for Annex.new, and making
sure that quiesce is called, after any action that might possibly need
it.

After that audit, I'm pretty sure that the change to Annex.run' is
safe. The only concern might be that this does let more changes get

Build/DistributionUpdate.hs
@@ -24,6 +24,7 @@ import Annex.Content
 import Annex.WorkTree
 import Git.Command
 import qualified Utility.RawFilePath as R
+import Annex.Actions
 
 import Data.Time.Clock
 import Data.Char
@@ -70,6 +71,7 @@ main = do
 	ood <- Annex.eval state $ do
 		buildrpms topdir updated
 		makeinfos updated version
+		quiesce False
 	syncToArchiveOrg
 	unless (null ood) $
 		error $ "Some info files are out of date: " ++ show (map fst ood)

Annex.hs
@@ -287,12 +287,8 @@ run (st, rd) a = do
 run' :: MVar AnnexState -> AnnexRead -> Annex a -> IO (a, (AnnexState, AnnexRead))
 run' mvar rd a = do
 	r <- runReaderT (runAnnex a) (mvar, rd)
-		`onException` (flush rd)
-	flush rd
 	st <- takeMVar mvar
 	return (r, (st, rd))
-  where
-	flush = Keys.flushDbQueue . keysdbhandle
 
 {- Performs an action in the Annex monad from a starting state, 
  - and throws away the changed state. -}

Annex/Action.hs
@@ -1,6 +1,6 @@
 {- git-annex actions
  -
- - Copyright 2010-2020 Joey Hess <id@joeyh.name>
+ - Copyright 2010-2022 Joey Hess <id@joeyh.name>
  -
  - Licensed under the GNU AGPL version 3 or higher.
  -}
@@ -11,7 +11,7 @@ module Annex.Action (
 	action,
 	verifiedAction,
 	startup,
-	shutdown,
+	quiesce,
 	stopCoProcesses,
 ) where
 
@@ -25,6 +25,7 @@ import Annex.CheckAttr
 import Annex.HashObject
 import Annex.CheckIgnore
 import Annex.TransferrerPool
+import qualified Database.Keys
 
 import Control.Concurrent.STM
 #ifndef mingw32_HOST_OS
@@ -74,12 +75,25 @@ startup = do
        return ()
 #endif
 
-{- Cleanup actions. -}
-shutdown :: Bool -> Annex ()
-shutdown nocommit = do
+{- Rn all cleanup actions, save all state, stop all long-running child
+ - processes.
+ -
+ - This can be run repeatedly with other Annex actions run in between,
+ - but usually it is run only once at the end.
+ -
+ - When passed True, avoids making any commits to the git-annex branch,
+ - leaving changes in the journal for later commit.
+ -}
+quiesce :: Bool -> Annex ()
+quiesce nocommit = do
+	cas <- Annex.withState $ \st -> return 
+		( st { Annex.cleanupactions = mempty }
+		, Annex.cleanupactions st
+		)
+	sequence_ (M.elems cas)
 	saveState nocommit
-	sequence_ =<< M.elems <$> Annex.getState Annex.cleanupactions
 	stopCoProcesses
+	Database.Keys.closeDb
 
 {- Stops all long-running child processes, including git query processes. -}
 stopCoProcesses :: Annex ()

Annex/Content.hs
@@ -718,7 +718,7 @@ listKeys' keyloc want = do
 saveState :: Bool -> Annex ()
 saveState nocommit = doSideAction $ do
 	Annex.Queue.flush
-	Database.Keys.closeDb
+	Database.Keys.flushDb
 	unless nocommit $
 		whenM (annexAlwaysCommit <$> Annex.getGitConfig) $
 			Annex.Branch.commit =<< Annex.Branch.commitMessage

Assistant/MakeRepo.hs
@@ -49,7 +49,7 @@ inDir dir a = do
 	state <- Annex.new
 		=<< Git.Config.read
 		=<< Git.Construct.fromPath (toRawFilePath dir)
-	Annex.eval state $ a `finally` stopCoProcesses
+	Annex.eval state $ a `finally` quiesce True
 
 {- Creates a new repository, and returns its UUID. -}
 initRepo :: Bool -> Bool -> FilePath -> Maybe String -> Maybe StandardGroup -> IO UUID

CmdLine.hs
@@ -63,7 +63,7 @@ dispatch' subcommandname args fuzzy cmds allargs allcmds fields getgitrepo progn
 			prepRunCommand cmd annexsetter
 			startup
 			performCommandAction True cmd seek $
-				shutdown $ cmdnocommit cmd
+				quiesce $ cmdnocommit cmd
 	go (Left norepo) = do
 		let ingitrepo = \a -> a =<< Git.Config.global
 		-- Parse command line with full cmdparser first,

CmdLine/GitRemoteTorAnnex.hs
@@ -17,6 +17,7 @@ import Utility.AuthToken
 import Annex.UUID
 import P2P.Address
 import P2P.Auth
+import Annex.Action
 
 run :: [String] -> IO ()
 run (_remotename:address:[]) = forever $
@@ -59,6 +60,8 @@ connectService address port service = do
 		g <- Annex.gitRepo
 		conn <- liftIO $ connectPeer g (TorAnnex address port)
 		runst <- liftIO $ mkRunState Client
-		liftIO $ runNetProto runst conn $ auth myuuid authtoken noop >>= \case
+		r <- liftIO $ runNetProto runst conn $ auth myuuid authtoken noop >>= \case
 			Just _theiruuid -> connect service stdin stdout
 			Nothing -> giveup $ "authentication failed, perhaps you need to set " ++ p2pAuthTokenEnv
+		quiesce False
+		return r

Command/RecvKey.hs
@@ -31,9 +31,7 @@ start (_, key) = fieldTransfer Download key $ \_p -> do
 	ifM (getViaTmp rsp DefaultVerify key (AssociatedFile Nothing) go)
 		( do
 			logStatus key InfoPresent
-			-- forcibly quit after receiving one key,
-			-- and shutdown cleanly
-			_ <- shutdown True
+			_ <- quiesce True
 			return True
 		, return False
 		)

commit bc980815eea3df9aaa349bc5a9e302461ac603b9

fix build of helper program
broken by ba7ecbc6a9c3763e8152e4f46522d14a4ee2b59d

Sponsored-by: Svenne Krap on Patreon

Build/DistributionUpdate.hs
@@ -24,7 +24,7 @@ import Annex.Content
 import Annex.WorkTree
 import Git.Command
 import qualified Utility.RawFilePath as R
-import Annex.Actions
+import Annex.Action
 
 import Data.Time.Clock
 import Data.Char
@@ -70,8 +70,9 @@ main = do
 	state <- Annex.new =<< Git.Construct.fromPath (toRawFilePath ".")
 	ood <- Annex.eval state $ do
 		buildrpms topdir updated
-		makeinfos updated version
+		is <- makeinfos updated version
 		quiesce False
+		return is
 	syncToArchiveOrg
 	unless (null ood) $
 		error $ "Some info files are out of date: " ++ show (map fst ood)