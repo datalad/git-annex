datalad/git-annex

commit 64fc34b3dab4c3ae1a623d1dce8e8b1110045e33

narrow window where HEAD is detached
Updating an adjusted branch can take a while when there are a lot of
files. HEAD was detached at the start, so if eg git-annex sync was
interrupted at the wrong point, there was a possibly wide window where
it would leave the repo with HEAD detached.

There's still a window, just much narrower. I don't know if it's
possible to close the window entirely. While git can clearly update
the currently checked out branch in eg git merge, it doesn't seem to
provide another way to do it.

Sponsored-by: Graham Spencer on Patreon

Annex/AdjustedBranch.hs
@@ -254,6 +254,8 @@ updateAdjustedBranch adj (AdjBranch currbranch) origbranch
 			-- origbranch.
 			_ <- propigateAdjustedCommits' origbranch adj commitlck
 			
+			b <- adjustBranch adj origbranch
+			
 			origheadfile <- inRepo $ readFileStrict . Git.Ref.headFile
 
 			-- Git normally won't do anything when asked to check
@@ -268,7 +270,6 @@ updateAdjustedBranch adj (AdjBranch currbranch) origbranch
 						return (Just newheadfile)
 				_ -> return Nothing
 	
-			b <- adjustBranch adj origbranch
 			return (b, origheadfile, newheadfile)
 	
 		-- Make git checkout quiet to avoid warnings about

commit 637f19bebba21b34980657bdf658d96ad8e2da0c

fix adjusted branch update breakage
Introduced recently in commit 64fc34b3dab4c3ae1a623d1dce8e8b1110045e33.

adjustBranch changes the sha that is recorded for the current branch
(eg the adjusted branch). So, have to get the original sha before
calling it.

Sponsored-by: Jack Hill on Patreon

Annex/AdjustedBranch.hs
@@ -254,18 +254,19 @@ updateAdjustedBranch adj (AdjBranch currbranch) origbranch
 			-- origbranch.
 			_ <- propigateAdjustedCommits' origbranch adj commitlck
 			
-			b <- adjustBranch adj origbranch
-			
 			origheadfile <- inRepo $ readFileStrict . Git.Ref.headFile
+			origheadsha <- inRepo (Git.Ref.sha currbranch)
+			
+			b <- adjustBranch adj origbranch
 
 			-- Git normally won't do anything when asked to check
 			-- out the currently checked out branch, even when its
 			-- ref has changed. Work around this by writing a raw
 			-- sha to .git/HEAD.
-			newheadfile <- inRepo (Git.Ref.sha currbranch) >>= \case
-				Just headsha -> do
+			newheadfile <- case origheadsha of
+				Just s -> do
 					inRepo $ \r -> do
-						let newheadfile = fromRef headsha
+						let newheadfile = fromRef s
 						writeFile (Git.Ref.headFile r) newheadfile
 						return (Just newheadfile)
 				_ -> return Nothing