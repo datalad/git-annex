datalad/git-annex

commit b6642dde8a49f34d7983a40e99bf7032a731c964

avoid interleaving command stages with Concurrency 1
Before, -J1 was different than no -J: It makes concurrent-output be used
for display, and it actually can run concurrent jobs in some situations.
Eg, a perform stage and a cleanup stage can both run.

dupState is used in several places, and changes NonConcurrent to
Concurrent 1. My concern is that this might, in some case, enable
that concurrent behavior. And in particular, that it might get enabled in
--batch mode, when the user is not expecting concurrent output because
they did not pass -J.

While I don't have a test case where that happens and causes out of
order output, it looks like it could, and so prudent to make this change.

CmdLine/Action.hs
@@ -55,7 +55,9 @@ commandActions = mapM_ commandAction
 commandAction :: CommandStart -> Annex ()
 commandAction start = getConcurrency >>= \case
 	NonConcurrent -> runnonconcurrent
-	Concurrent _ -> runconcurrent
+	Concurrent n
+		| n > 1 -> runconcurrent
+		| otherwise -> runnonconcurrent
 	ConcurrentPerCpu -> runconcurrent
   where
 	runnonconcurrent = void $ includeCommandAction start

commit 114a2d7504c94e791218431abc1edbe7d21f9fa2

Fix display when run with -J1
Commit b6642dde8a49f34d7983a40e99bf7032a731c964 broke it by enabling
non-concurrent display mode while leaving concurrency set in the config
and having already started concurrency earlier.

(I don't actually know if that commit was a good idea.)

Sponsored-By: Brett Eisenberg on Patreon

CmdLine/Action.hs
@@ -74,9 +74,7 @@ commandAction start = do
 	st <- Annex.getState id
 	case getConcurrency' (Annex.concurrency st) of
 		NonConcurrent -> runnonconcurrent (Annex.sizelimit st)
-		Concurrent n
-			| n > 1 -> runconcurrent (Annex.sizelimit st) (Annex.workers st)
-			| otherwise -> runnonconcurrent (Annex.sizelimit st)
+		Concurrent _ -> runconcurrent (Annex.sizelimit st) (Annex.workers st)
 		ConcurrentPerCpu -> runconcurrent (Annex.sizelimit st) (Annex.workers st)
   where
 	runnonconcurrent sizelimit = start >>= \case
@@ -235,8 +233,11 @@ startConcurrency usedstages a = do
 	let usegitcfg = setConcurrency (ConcurrencyGitConfig fromgitcfg)
 	case (fromcmdline, fromgitcfg) of
 		(NonConcurrent, NonConcurrent) -> a
-		(Concurrent n, _) ->
-			goconcurrent n
+		(Concurrent n, _) 
+			| n > 1 -> goconcurrent n
+			| otherwise -> do
+				setConcurrency' NonConcurrent ConcurrencyCmdLine
+				a
 		(ConcurrentPerCpu, _) ->
 			goconcurrentpercpu
 		(NonConcurrent, Concurrent n) -> do

doc/bugs/J1_display_issue.mdwn
@@ -9,3 +9,5 @@ The filename is displayed at end.
 This does not happen at -J2 or without -J.
 
 --[[Joey]]
+
+> [[fixed|done]] --[[Joey]]