393275c10521145bcc6b18cfd39db6854ae0831e
Joey Hess <joeyh@joeyh.name>
2023-08-01 15:08:56 -0400

    Setup.hs: Stop installing man pages, desktop files, and the git-annex-shell and git-remote-tor-annex symlinks
    Anything still relying on that, eg via cabal v1-install will need to
    change to using make install-home. Which was added back in 2019 in
    6491b626140a9e5cc8e99fff4a1434d7c0e9bc39 because cabal new-build
    (now the default) already didn't use Setup in a way that let its
    installation of those things work.
    
    Notably this means Setup does not need to depend on unix-compat, which is
    useful because in 0.7 it removed System.PosixCompat.User, which Setup
    needed to determine where to install the desktop files. See
    https://github.com/haskell-pkg-janitors/unix-compat/issues/3

--- Setup.hs
+++ Setup.hs
@@ -1,72 +1,13 @@
-{-# LANGUAGE NamedFieldPuns #-}
 {-# OPTIONS_GHC -fno-warn-tabs #-}
 
 {- cabal setup file -}
 
 import Distribution.Simple
-import Distribution.Simple.LocalBuildInfo
-import Distribution.Simple.Setup
-import Distribution.Simple.Utils (installOrdinaryFiles, rawSystemExit)
-import Distribution.PackageDescription (PackageDescription(..))
-import Distribution.Verbosity (Verbosity)
-import System.FilePath
-import Control.Applicative
-import Control.Monad
-import System.Directory
-import Data.List
-import Data.Maybe
-import Control.Exception
-import qualified System.Info
-
-import qualified Build.DesktopFile as DesktopFile
 import qualified Build.Configure as Configure
-import Build.Mans (buildMans)
-import Utility.SafeCommand
 
 main :: IO ()
 main = defaultMainWithHooks simpleUserHooks
 	{ preConf = \_ _ -> do
 		Configure.run Configure.tests
 		return (Nothing, [])	
-	, postCopy = myPostCopy
 	}
-
-myPostCopy :: Args -> CopyFlags -> PackageDescription -> LocalBuildInfo -> IO ()
-myPostCopy _ flags pkg lbi = when (System.Info.os /= "mingw32") $ do
-	installGitAnnexLinks dest verbosity pkg lbi
-	installManpages      dest verbosity pkg lbi
-	installDesktopFile   dest verbosity pkg lbi
-  where
-	dest      = fromFlag $ copyDest flags
-	verbosity = fromFlag $ copyVerbosity flags
-
-installGitAnnexLinks :: CopyDest -> Verbosity -> PackageDescription -> LocalBuildInfo -> IO ()
-installGitAnnexLinks copyDest verbosity pkg lbi = do
-	rawSystemExit verbosity "ln"
-		["-sf", "git-annex", dstBinDir </> "git-annex-shell"]
-	rawSystemExit verbosity "ln"
-		["-sf", "git-annex", dstBinDir </> "git-remote-tor-annex"]
-  where
-	dstBinDir = bindir $ absoluteInstallDirs pkg lbi copyDest
-
-{- See http://www.haskell.org/haskellwiki/Cabal/Developer-FAQ#Installing_manpages -}
-installManpages :: CopyDest -> Verbosity -> PackageDescription -> LocalBuildInfo -> IO ()
-installManpages copyDest verbosity pkg lbi =
-	installOrdinaryFiles verbosity dstManDir =<< srcManpages
-  where
-	dstManDir   = mandir (absoluteInstallDirs pkg lbi copyDest) </> "man1"
-	-- If mdwn2man fails, perhaps because perl is not available,
-	-- we just skip installing man pages.
-	srcManpages = zip (repeat "man") . map takeFileName . catMaybes
-		<$> buildMans
-
-installDesktopFile :: CopyDest -> Verbosity -> PackageDescription -> LocalBuildInfo -> IO ()
-installDesktopFile copyDest _verbosity pkg lbi
-	| progfile copyDest == progfile NoCopyDest =
-		DesktopFile.installUser (progfile copyDest)
-			`catch` installerror
-	| otherwise = return ()
-  where
-	progfile cd = bindir (absoluteInstallDirs pkg lbi cd) </> "git-annex"
-	installerror :: SomeException -> IO ()
-	installerror e = putStrLn ("Warning: Installation of desktop integration files did not succeed (" ++ show e ++ "); skipping.")
