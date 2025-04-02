bb04d1e71fe4a9c50c7e34a65a5687aca62e3ed5
Joey Hess <joeyh@joeyh.name>
2025-04-02 15:29:46 -0400

    URL decoding for username and path
    * Support git remotes that use an url with a user name that is URL encoded.
    * Fix git-lfs special remote ssh endpoint discovery when the repository
      path is URL encoded.
    
    In the previous commit, Git.Url.host was made to do URL decoding. That made
    me wonder, what about URL encoded username and path? And so to these two
    additional fixes. Note that Git.Url.authority remains URL encoded. That
    seems ok given how it's used.

--- Git/Url.hs
+++ Git/Url.hs
@@ -54,10 +54,13 @@ port r =
 		Just (':':p) -> readish p
 		Just _ -> Nothing
 
-{- Hostname of an URL repo, including any username (ie, "user@host") -}
+{- Hostname of an URL repo, including any username (ie, "user@host")
+ -
+ - Both the username and hostname are URI-decoded.
+ -}
 hostuser :: Repo -> Maybe String
 hostuser r = (++)
-	<$> authpart uriUserInfo r
+	<$> authpart (unEscapeString . uriUserInfo) r
 	<*> host r
 
 {- The full authority portion an URL repo. (ie, "user@host:port") -}
@@ -71,7 +74,7 @@ authpart :: (URIAuth -> a) -> Repo -> Maybe a
 authpart a Repo { location = Url u } = a <$> uriAuthority u
 authpart _ _ = Nothing
 
-{- Path part of an URL repo. -}
+{- Path part of an URL repo. It is URI-decoded. -}
 path :: Repo -> Maybe FilePath
-path Repo { location = Url u } = Just (uriPath u)
+path Repo { location = Url u } = Just $ unEscapeString $ uriPath u
 path _ = Nothing
