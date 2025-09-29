17c86e50c05be52715e90ebc71a5a5cfc5c788e7
Joey Hess <joeyh@joeyh.name>
2025-09-29 12:42:48 -0400

    webapp: Remove support for local pairing
    As a feature only supported by the webapp, and not by git-annex at the
    command line, this is by now a very obscure corner of git-annex, and not
    one I want to keep maintaining.
    
    It's worth removing it to avoid the security expsure alone. People using
    the assistant w/o the webapp probably don't expect it to be listening on
    a UDP port for a handrolled protocol, but it was.
    
    The webapp has supported pairing via magic-wormhole since 2016, which
    makes a link including between local computers, albeit with the overhead
    of tor. That sort of covers the same use case. Of course advanced users
    can easily enough add a ssh remote to their repository themselves, using
    a hostname on the local network.
    
    git-annex-p2p-iroh would be a great alternative, since it should
    communicate over LAN when both computers are on the same one. Before
    supporting that in the webapp, dumbpipe would need to be reasonably
    likely to be installed.
    
    Sponsored-by: unqueued

--- BuildFlags.hs
+++ BuildFlags.hs
@@ -26,11 +26,6 @@ buildFlags = filter (not . null)
 #else
 #warning Building without the webapp.
 #endif
-#ifdef WITH_PAIRING
-	, "Pairing"
-#else
-#warning Building without local pairing.
-#endif
 #ifdef WITH_INOTIFY
 	, "Inotify"
 #endif
