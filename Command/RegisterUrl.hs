cd32ac00a16c44894b9d91152c986ab066545fcb
Joey Hess <joeyh@joeyh.name>
2026-01-27 13:35:50 -0400

    fix typo

--- Command/RegisterUrl.hs
+++ Command/RegisterUrl.hs
@@ -37,7 +37,7 @@ seek o = case (batchOption o, keyUrlPairs o) of
 	(Batch fmt, _) -> seekBatch registerUrl o fmt
 	-- older way of enabling batch input, does not support BatchNull
 	(NoBatch, []) -> seekBatch registerUrl o (BatchFormat BatchLine (BatchKeys False))
-	(NoBatch, ps) -> commandAction (start "unregisterurl" registerUrl o ps)
+	(NoBatch, ps) -> commandAction (start "registerurl" registerUrl o ps)
 
 seekBatch :: (Remote -> Key -> URLString -> Annex ()) -> RegisterUrlOptions -> BatchFormat -> CommandSeek
 seekBatch a o fmt = batchOnly Nothing (keyUrlPairs o) $
