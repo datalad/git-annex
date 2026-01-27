3cf66648b8f1d9884485fdbdd8f8428604743d3d
Joey Hess <joeyh@joeyh.name>
2026-01-27 13:21:52 -0400

    unregisterurl: Unregister both VURL and URL keys when passed an url instead of a key
    The idea with doing both is that unregisterurl is used with an url when
    the content of the url is no longer present. So unregistering both makes
    sense. And, as git-annex transitions from using URL to VURL by default,
    there can be both in repos, and so unregistering both avoids breaking
    workflows that used to register URL keys, but are now registering VURL
    keys.

--- Backend/URL.hs
+++ Backend/URL.hs
@@ -1,13 +1,14 @@
 {- git-annex URL backend -- keys whose content is available from urls.
  -
- - Copyright 2011-2024 Joey Hess <id@joeyh.name>
+ - Copyright 2011-2026 Joey Hess <id@joeyh.name>
  -
  - Licensed under the GNU AGPL version 3 or higher.
  -}
 
 module Backend.URL (
 	backends,
-	fromUrl
+	fromUrl,
+	otherUrlKey,
 ) where
 
 import Annex.Common
@@ -41,3 +42,12 @@ fromUrl url size verifiable = mkKey $ \k -> k
 	, keyVariety = if verifiable then VURLKey else URLKey
 	, keySize = size
 	}
+
+{- From an URL key to a VURL key and vice-versa. -}
+otherUrlKey :: Key -> Maybe Key
+otherUrlKey k
+	| fromKey keyVariety k == URLKey = Just $
+		alterKey k $ \kd -> kd { keyVariety = VURLKey }
+	| fromKey keyVariety k == VURLKey = Just $
+		alterKey k $ \kd -> kd { keyVariety = URLKey }
+	| otherwise = Nothing
--- Command/UnregisterUrl.hs
+++ Command/UnregisterUrl.hs
@@ -1,6 +1,6 @@
 {- git-annex command
  -
- - Copyright 2015-2022 Joey Hess <id@joeyh.name>
+ - Copyright 2015-2026 Joey Hess <id@joeyh.name>
  -
  - Licensed under the GNU AGPL version 3 or higher.
  -}
@@ -12,6 +12,7 @@ module Command.UnregisterUrl where
 import Command
 import Logs.Web
 import Command.RegisterUrl (seekBatch, start, optParser, RegisterUrlOptions(..))
+import Backend.URL
 
 cmd :: Command
 cmd = withAnnexOptions [jsonOptions] $ command "unregisterurl"
@@ -26,13 +27,19 @@ seek o = case (batchOption o, keyUrlPairs o) of
 
 unregisterUrl :: Remote -> Key -> String -> Annex ()
 unregisterUrl _remote key url = do
+	unregisterUrl' url key
+	maybe noop (unregisterUrl' url) (otherUrlKey key)
+
+unregisterUrl' :: String -> Key -> Annex ()
+unregisterUrl' url key = do
 	-- Remove the url no matter what downloader;
 	-- registerurl can set OtherDownloader, and this should also
 	-- be able to remove urls added by addurl, which may use
 	-- YoutubeDownloader.
 	forM_ [minBound..maxBound] $ \dl ->
 		setUrlMissing key (setDownloader url dl)
-	-- Unlike unregisterurl, this does not update location tracking
-	-- for remotes other than the web special remote. Doing so with
-	-- a remote that git-annex can drop content from would rather
-	-- unexpectedly leave content stranded on that remote.
+	-- Unlike registerurl, this does not update location
+	-- tracking for remotes other than the web special remote.
+	-- Doing so with a remote that git-annex can drop content
+	-- from would rather unexpectedly leave content stranded
+	-- on that remote.
